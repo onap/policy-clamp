2ff12cf396e3ea6ab166e8c9bc7fc1cab93e8cd5
Determe, Sebastien (sd378r) <sd378r@intl.att.com>
2018-03-16 19:42:39 +0100

    Add new config classes
    Add classes to support the Blueprint mapping configuration
    
    Issue-ID: CLAMP-81
    Change-Id: I78b42f90993700c299fd75a20f39aa16f632ff9d
    Signed-off-by: Determe, Sebastien (sd378r) <sd378r@intl.att.com>

---
+++ src/main/java/org/onap/clamp/clds/config/sdc/BlueprintParserFilesConfiguration.java
@@ -0,0 +1,52 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ */
+
+package org.onap.clamp.clds.config.sdc;
+
+/**
+ * This class is used to decode the configuration found in
+ * application.properties, this is related to the blueprint mapping
+ * configuration that is used to create data in database, according to the
+ * blueprint content coming from SDC.
+ */
+public class BlueprintParserFilesConfiguration {
+
+    private String svgXmlFilePath;
+    private String bpmnXmlFilePath;
+
+    public String getBpmnXmlFilePath() {
+        return bpmnXmlFilePath;
+    }
+
+    public void setBpmnXmlFilePath(String bpmnXmlFilePath) {
+        this.bpmnXmlFilePath = bpmnXmlFilePath;
+    }
+
+    public String getSvgXmlFilePath() {
+        return svgXmlFilePath;
+    }
+
+    public void setSvgXmlFilePath(String svgXmlFilePath) {
+        this.svgXmlFilePath = svgXmlFilePath;
+    }
+}
