6044573a296569e93323e39353da4295cfc69c09
Determe, Sebastien (sd378r) <sd378r@intl.att.com>
2018-04-04 12:03:37 +0200

    Fix the python startup script
    Fix the startup script for python simulator
    
    Issue-ID: CLAMP-148
    Change-Id: Ia1906aa12f2354a45324553609a9e5e713d8a551
    Signed-off-by: Determe, Sebastien (sd378r) <sd378r@intl.att.com>

--- src/test/resources/http-cache/start_http_cache.sh
+++ src/test/resources/http-cache/start_http_cache.sh
@@ -22,23 +22,30 @@
 # ECOMP is a trademark and service mark of AT&T Intellectual Property.
 ###
 
-if [ $# -eq 2 ]
-	then
-		echo 'Setting http_proxy and proxyaddress script parameters';
-		export http_proxy=$1
-		export https_proxy=$1
-		python_proxyaddress=$2
-		echo 'http_proxy was set to '$http_proxy
-		echo 'python_proxyaddress was set to '$python_proxyaddress
-	else
-		echo 'Required parameters are not set';
-		echo 'Command Format:  start_http_cache.sh <http_proxy_adress> <host_running_test:port>';
-		echo '  http_proxy_adress, like http://my.proxy.com:8080 and will be set to http_proxy/https_proxy environment variables';
-		echo '  host_running_test, like localhost:8080 and will be set as --proxyaddress, this is the adress returned by DCAE simulator response';
-		exit 1
-fi
+for i in "$@"
+do
+case $i in
+    --python_proxyaddress=*)
+      python_proxyaddress="--proxyaddress ${i#*=}"
+      echo "- Using python_proxyaddress and set it to: $python_proxyaddress"
+      shift # past argument=value
+      ;;
+    --http_proxyaddress=*)
+      export http_proxy="${i#*=}"
+      export https_proxy=$http_proxy
+      echo "- Defining http_proxy/https_proxy env variables to: $http_proxy"
+      shift # past argument=value
+      ;;
+     -?|--help|-help)
+      echo "Usage: $(basename $0) [--http_proxyaddress=<http://proxy_address:port>] [--python_proxyaddress=<python_simulator_address:port>]"
+      echo "--http_proxyaddress Set the http_proxy/https_proxy in the script before running python"
+      echo "--python_proxyaddress <python_simulator_address:port>, like localhost:8080 and will be set as --proxyaddress, this is the adress returned by DCAE simulator response"
+      exit 2
+      ;;
+esac
+done
 
 echo 'Installing requests packages for Python'
 pip install requests
 echo 'Executing the Http proxy in Cache mode only'
-python third_party_proxy.py --port 8080 --root /usr/src/http-cache-app/data-cache --proxyaddress $python_proxyaddress
+python third_party_proxy.py --port 8080 --root /usr/src/http-cache-app/data-cache $python_proxyaddress
