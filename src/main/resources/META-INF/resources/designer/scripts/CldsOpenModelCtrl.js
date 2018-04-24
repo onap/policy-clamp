cadb2cb688a65eefa741c827ef0f85932e49267f
Determe, Sebastien (sd378r) <sd378r@intl.att.com>
2018-04-24 18:11:02 +0200

    Remove ECOMP in headers
    Remove ECOMP in headers of the source files
    
    Issue-ID: CLAMP-156
    Change-Id: I8bf77f4c0cf884bdab2f9f1c1d56f91d98c6967d
    Signed-off-by: Determe, Sebastien (sd378r) <sd378r@intl.att.com>

--- src/main/resources/META-INF/resources/designer/scripts/CldsOpenModelCtrl.js
+++ src/main/resources/META-INF/resources/designer/scripts/CldsOpenModelCtrl.js
@@ -18,7 +18,7 @@
  * limitations under the License.
  * ============LICENSE_END============================================
  * ===================================================================
- * ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * 
  */
 app.controller('CldsOpenModelCtrl',
 	['$scope', '$rootScope', '$modalInstance','cldsModelService', '$location', 'dialogs','cldsTemplateService',
