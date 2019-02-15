2f468e4472e56ec56cb9823d188a564749b93457
sebdet <sebastien.determe@intl.att.com>
2019-02-15 15:29:54 +0100

    Remove remaining Jackson
    Remove jackson import as jackson shoul dbe removed
    
    Issue-ID: CLAMP-292
    Change-Id: I43b3bd2e2f217293ae8ac262b013b2287d1e2b16
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- src/main/java/org/onap/clamp/clds/util/JsonUtils.java
+++ src/main/java/org/onap/clamp/clds/util/JsonUtils.java
@@ -31,6 +31,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -38,12 +39,13 @@ import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
+
 import org.onap.clamp.clds.model.properties.AbstractModelElement;
 import org.onap.clamp.clds.service.SecureServicePermission;
 import org.onap.clamp.clds.service.SecureServicePermissionDeserializer;
 
 /**
- * This class is used to access the jackson with restricted type access.
+ * This class is used to access the GSON with restricted type access.
  */
 public class JsonUtils {
 
@@ -52,19 +54,17 @@ public class JsonUtils {
     private static final String LOG_ELEMENT_NOT_FOUND_IN_JSON = "Value '{}' for key 'name' not found in JSON {}";
 
     public static final Gson GSON = new GsonBuilder()
-        .registerTypeAdapter(SecureServicePermission.class, new SecureServicePermissionDeserializer())
-        .create();
+        .registerTypeAdapter(SecureServicePermission.class, new SecureServicePermissionDeserializer()).create();
 
     private JsonUtils() {
     }
 
-
     /**
-     * Return the value field of the json node element that has a name field equals to the given name.
+     * Return the value field of the json node element that has a name field equals
+     * to the given name.
      */
     public static String getStringValueByName(JsonElement jsonElement, String name) {
-        String value = extractJsonValueFromElement(jsonElement, name)
-            .map(JsonUtils::extractStringValueFromElement)
+        String value = extractJsonValueFromElement(jsonElement, name).map(JsonUtils::extractStringValueFromElement)
             .orElse(null);
         if (value == null) {
             if (logger.isDebugEnabled()) {
@@ -77,7 +77,8 @@ public class JsonUtils {
     }
 
     /**
-     * Return an array of values for the field of the json node element that has a name field equals to the given name.
+     * Return an array of values for the field of the json node element that has a
+     * name field equals to the given name.
      */
     public static List<String> getStringValuesByName(JsonElement jsonElement, String name) {
         List<String> values = extractJsonValueFromElement(jsonElement, name)
@@ -93,21 +94,22 @@ public class JsonUtils {
     }
 
     /**
-     * Return the int value field of the json node element that has a name field equals to the given name.
+     * Return the int value field of the json node element that has a name field
+     * equals to the given name.
      */
     public static Integer getIntValueByName(JsonElement element, String name) {
         String value = getStringValueByName(element, name);
         return Integer.valueOf(value);
     }
 
-
     /**
-     * Return the Json value field of the json node element that has a name field equals to the given name.
+     * Return the Json value field of the json node element that has a name field
+     * equals to the given name.
      */
     public static JsonObject getJsonObjectByName(JsonElement jsonElement, String name) {
         JsonObject jsonObject = extractJsonValueFromElement(jsonElement, name).map(JsonElement::getAsJsonObject)
             .orElse(null);
-        if (jsonObject  == null) {
+        if (jsonObject == null) {
             logger.warn(LOG_ELEMENT_NOT_FOUND, name);
         } else {
             logger.debug(LOG_ELEMENT_NOT_FOUND_IN_JSON, name, jsonElement.toString());
@@ -116,7 +118,7 @@ public class JsonUtils {
     }
 
     private static Optional<JsonElement> extractJsonValueFromElement(JsonElement jsonElement, String name) {
-        if(jsonElement != null ){
+        if (jsonElement != null) {
             if (jsonElement.isJsonArray()) {
                 return extractValueJsonFromArray(jsonElement, name);
             } else if (hasMatchingParameterName(name, jsonElement)) {
@@ -136,8 +138,7 @@ public class JsonUtils {
     }
 
     private static boolean hasMatchingParameterName(String name, JsonElement element) {
-        return element.isJsonObject()
-            && element.getAsJsonObject().has("name")
+        return element.isJsonObject() && element.getAsJsonObject().has("name")
             && name.equals(element.getAsJsonObject().get("name").getAsString());
     }
 
@@ -153,14 +154,11 @@ public class JsonUtils {
 
     private static List<String> extractStringValuesFromElement(JsonElement element) {
         if (element.isJsonArray()) {
-            return StreamSupport.stream(
-                Spliterators.spliteratorUnknownSize(element.getAsJsonArray().iterator(), Spliterator.ORDERED),
-                false)
-                .filter(JsonElement::isJsonPrimitive)
-                .map(JsonElement::getAsJsonPrimitive)
-                .filter(JsonPrimitive::isString)
-                .map(JsonPrimitive::getAsString)
-                .collect(Collectors.toList());
+            return StreamSupport
+                .stream(Spliterators.spliteratorUnknownSize(element.getAsJsonArray().iterator(), Spliterator.ORDERED),
+                    false)
+                .filter(JsonElement::isJsonPrimitive).map(JsonElement::getAsJsonPrimitive)
+                .filter(JsonPrimitive::isString).map(JsonPrimitive::getAsString).collect(Collectors.toList());
         } else {
             String value = extractStringValueFromElement(element);
             return Lists.newArrayList(value);
