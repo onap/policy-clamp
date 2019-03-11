onap/clamp

Convert modules and relations to SVG
Generate SVG image from modules chain and save it to DB.

Change-Id: I0f0b065f82de9b4d2084ec51043f485401842e23
Issue-ID: CLAMP-284
Signed-off-by: Piotr Darosz <piotr.darosz@nokia.com>

src/main/java/org/onap/clamp/clds/util/drawing/ClampGraph.java
@@ -1,0 +1,43 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 Nokia. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.clamp.clds.util.drawing;
+
+import java.util.Objects;
+import org.onap.clamp.clds.util.XmlTools;
+
+public class ClampGraph {
+    private final DocumentBuilder documentBuilder;
+    private String svg;
+
+    ClampGraph(DocumentBuilder documentBuilder) {
+        this.documentBuilder = documentBuilder;
+    }
+
+    public String getAsSVG() {
+        if(Objects.isNull(svg) || svg.isEmpty()) {
+            svg = XmlTools.exportXmlDocumentAsString(this.documentBuilder.getDocument());
+        }
+        return svg;
+    }
+}

src/main/java/org/onap/clamp/clds/util/drawing/ClampGraphBuilder.java
@@ -1,0 +1,64 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 Nokia. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.clamp.clds.util.drawing;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ClampGraphBuilder {
+    private String policy;
+    private String collector;
+    private List<String> microServices = new ArrayList<>();
+    private final Painter painter;
+
+    public ClampGraphBuilder(Painter p) {
+        painter = p;
+    }
+
+    public ClampGraphBuilder collector(String c) {
+        collector = c;
+        return this;
+    }
+
+    public ClampGraphBuilder policy(String p) {
+        policy = p;
+        return this;
+    }
+
+    public ClampGraphBuilder microService(String ms) {
+        microServices.add(ms);
+        return this;
+    }
+
+    public ClampGraph build() {
+        if(microServices.isEmpty()) {
+            throw new InvalidStateException("At least one microservice is required");
+        }
+        if(Objects.isNull(policy) || policy.trim().isEmpty()) {
+            throw new InvalidStateException("Policy element must be present");
+        }
+        return new ClampGraph(painter.doPaint(collector, microServices, policy));
+    }
+}

src/test/java/org/onap/clamp/clds/util/drawing/ClampGraphTest.java
@@ -1,0 +1,77 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 Nokia. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.clamp.clds.util.drawing;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.onap.clamp.clds.util.ResourceFileUtil;
+import org.onap.clamp.clds.util.XmlToolsTest;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ClampGraphTest {
+    @Mock
+    private DocumentBuilder mockDocumentBuilder;
+
+    @Test
+    public void getAsSVGTest() throws IOException, ParserConfigurationException, SAXException {
+        String expected = ResourceFileUtil.getResourceAsString("clds/util/file.xml");
+        Document document = XmlToolsTest.parseStringToXmlDocument(expected);
+
+        when(mockDocumentBuilder.getDocument()).thenReturn(document);
+
+        String actual = new ClampGraph(mockDocumentBuilder).getAsSVG();
+        Assert.assertEquals(expected.trim(), actual.trim());
+    }
+
+    @Test
+    public void getAsSVGLazyTest() throws IOException, ParserConfigurationException, SAXException {
+        String expected = ResourceFileUtil.getResourceAsString("clds/util/file.xml");
+        Document document = XmlToolsTest.parseStringToXmlDocument(expected);
+
+        when(mockDocumentBuilder.getDocument()).thenReturn(document);
+        ClampGraph cg = new ClampGraph(mockDocumentBuilder);
+
+        String actualFirst = cg.getAsSVG();
+        verify(mockDocumentBuilder, times(1)).getDocument();
+
+        String actualSecond = cg.getAsSVG();
+        verifyNoMoreInteractions(mockDocumentBuilder);
+
+        Assert.assertEquals(expected.trim(), actualFirst.trim());
+        Assert.assertEquals(expected.trim(), actualSecond.trim());
+
+    }
+}

src/test/java/org/onap/clamp/clds/util/drawing/DocumentBuilderTest.java
@@ -1,0 +1,80 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 Nokia. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.clamp.clds.util.drawing;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.apache.batik.util.SVGConstants;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.onap.clamp.clds.util.ResourceFileUtil;
+import org.onap.clamp.clds.util.XmlToolsTest;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DocumentBuilderTest {
+    @Mock
+    private SVGGraphics2D mockG2d;
+
+    @Mock
+    private Document mockDomImpl;
+
+    @Test
+    public void pushChangestoDocumentTest() throws IOException, ParserConfigurationException, SAXException {
+        String dataElementId = "someId";
+        String newNodeTag = "tagged";
+        String newNodeText = "Sample text";
+        String xml = ResourceFileUtil.getResourceAsString("clds/util/file.xml");
+        Document document = XmlToolsTest.parseStringToXmlDocument(xml);
+        Node newNode = document.createElement(newNodeTag);
+        newNode.appendChild(document.createTextNode(newNodeText));
+
+        when(mockG2d.getRoot(any(Element.class))).then(a -> a.getArgumentAt(0, Element.class).appendChild(newNode));
+
+        DocumentBuilder db = new DocumentBuilder(document, document);
+        db.pushChangestoDocument(mockG2d, dataElementId);
+        Document actualDocument = db.getDocument();
+
+        Node addedActualNode = actualDocument.getDocumentElement().getLastChild();
+        String actualDataElementId =
+            addedActualNode.getAttributes().getNamedItem(DocumentBuilder.DATA_ELEMENT_ID_ATTRIBUTE).getTextContent();
+
+        Assert.assertEquals(dataElementId, actualDataElementId);
+        Assert.assertEquals(SVGConstants.SVG_G_TAG, addedActualNode.getNodeName());
+
+        Node addedActualNodeChild = addedActualNode.getLastChild();
+        Assert.assertEquals(newNodeTag, addedActualNodeChild.getNodeName());
+        Assert.assertEquals(newNodeText, addedActualNodeChild.getTextContent());
+    }
+}

onap/policy-clamp

src/main/java/org/onap/clamp/clds/util/drawing/ClampGraph.java
@@ -1,0 +1,43 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 Nokia. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.clamp.clds.util.drawing;
+
+import java.util.Objects;
+import org.onap.clamp.clds.util.XmlTools;
+
+public class ClampGraph {
+    private final DocumentBuilder documentBuilder;
+    private String svg;
+
+    ClampGraph(DocumentBuilder documentBuilder) {
+        this.documentBuilder = documentBuilder;
+    }
+
+    public String getAsSVG() {
+        if(Objects.isNull(svg) || svg.isEmpty()) {
+            svg = XmlTools.exportXmlDocumentAsString(this.documentBuilder.getGroupingDocument());
+        }
+        return svg;
+    }
+}

src/main/java/org/onap/clamp/clds/util/drawing/ClampGraphBuilder.java
@@ -1,0 +1,64 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 Nokia. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.clamp.clds.util.drawing;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ClampGraphBuilder {
+    private String policy;
+    private String collector;
+    private List<String> microServices = new ArrayList<>();
+    private final Painter painter;
+
+    public ClampGraphBuilder(Painter painter) {
+        this.painter = painter;
+    }
+
+    public ClampGraphBuilder collector(String c) {
+        collector = c;
+        return this;
+    }
+
+    public ClampGraphBuilder policy(String p) {
+        policy = p;
+        return this;
+    }
+
+    public ClampGraphBuilder microService(String ms) {
+        microServices.add(ms);
+        return this;
+    }
+
+    public ClampGraph build() {
+        if(microServices.isEmpty()) {
+            throw new InvalidStateException("At least one microservice is required");
+        }
+        if(Objects.isNull(policy) || policy.trim().isEmpty()) {
+            throw new InvalidStateException("Policy element must be present");
+        }
+        return new ClampGraph(painter.doPaint(collector, microServices, policy));
+    }
+}

src/test/java/org/onap/clamp/clds/util/drawing/ClampGraphTest.java
@@ -1,0 +1,77 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 Nokia. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.clamp.clds.util.drawing;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.onap.clamp.clds.util.ResourceFileUtil;
+import org.onap.clamp.clds.util.XmlToolsTest;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ClampGraphTest {
+    @Mock
+    private DocumentBuilder mockDocumentBuilder;
+
+    @Test
+    public void getAsSVGTest() throws IOException, ParserConfigurationException, SAXException {
+        String expected = ResourceFileUtil.getResourceAsString("clds/util/file.xml");
+        Document document = XmlToolsTest.parseStringToXmlDocument(expected);
+
+        when(mockDocumentBuilder.getGroupingDocument()).thenReturn(document);
+
+        String actual = new ClampGraph(mockDocumentBuilder).getAsSVG();
+        Assert.assertEquals(expected.trim(), actual.trim());
+    }
+
+    @Test
+    public void getAsSVGLazyTest() throws IOException, ParserConfigurationException, SAXException {
+        String expected = ResourceFileUtil.getResourceAsString("clds/util/file.xml");
+        Document document = XmlToolsTest.parseStringToXmlDocument(expected);
+
+        when(mockDocumentBuilder.getGroupingDocument()).thenReturn(document);
+        ClampGraph cg = new ClampGraph(mockDocumentBuilder);
+
+        String actualFirst = cg.getAsSVG();
+        verify(mockDocumentBuilder, times(1)).getGroupingDocument();
+
+        String actualSecond = cg.getAsSVG();
+        verifyNoMoreInteractions(mockDocumentBuilder);
+
+        Assert.assertEquals(expected.trim(), actualFirst.trim());
+        Assert.assertEquals(expected.trim(), actualSecond.trim());
+
+    }
+}

src/test/java/org/onap/clamp/clds/util/drawing/DocumentBuilderTest.java
@@ -1,0 +1,80 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 Nokia. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.clamp.clds.util.drawing;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.apache.batik.util.SVGConstants;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.onap.clamp.clds.util.ResourceFileUtil;
+import org.onap.clamp.clds.util.XmlToolsTest;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DocumentBuilderTest {
+    @Mock
+    private SVGGraphics2D mockG2d;
+
+    @Mock
+    private Document mockDomImpl;
+
+    @Test
+    public void pushChangestoDocumentTest() throws IOException, ParserConfigurationException, SAXException {
+        String dataElementId = "someId";
+        String newNodeTag = "tagged";
+        String newNodeText = "Sample text";
+        String xml = ResourceFileUtil.getResourceAsString("clds/util/file.xml");
+        Document document = XmlToolsTest.parseStringToXmlDocument(xml);
+        Node newNode = document.createElement(newNodeTag);
+        newNode.appendChild(document.createTextNode(newNodeText));
+
+        when(mockG2d.getRoot(any(Element.class))).then(a -> a.getArgumentAt(0, Element.class).appendChild(newNode));
+
+        DocumentBuilder db = new DocumentBuilder(document, document);
+        db.pushChangestoDocument(mockG2d, dataElementId);
+        Document actualDocument = db.getGroupingDocument();
+
+        Node addedActualNode = actualDocument.getDocumentElement().getLastChild();
+        String actualDataElementId =
+            addedActualNode.getAttributes().getNamedItem(DocumentBuilder.DATA_ELEMENT_ID_ATTRIBUTE).getTextContent();
+
+        Assert.assertEquals(dataElementId, actualDataElementId);
+        Assert.assertEquals(SVGConstants.SVG_G_TAG, addedActualNode.getNodeName());
+
+        Node addedActualNodeChild = addedActualNode.getLastChild();
+        Assert.assertEquals(newNodeTag, addedActualNodeChild.getNodeName());
+        Assert.assertEquals(newNodeText, addedActualNodeChild.getTextContent());
+    }
+}