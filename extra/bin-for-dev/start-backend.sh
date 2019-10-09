7d277f0b30cd35b7e8d758863c8cc9eadb62eecd
sebdet <sebastien.determe@intl.att.com>
2019-10-09 14:30:36 +0200

    Fix start-backend script
    Just ensure the log folder is well there before starting the backend
    
    Issue-ID: CLAMP-535
    Change-Id: I355ecba7dad0fa6f272a69ad6c89c422b6551094
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- extra/bin-for-dev/start-backend.sh
+++ extra/bin-for-dev/start-backend.sh
@@ -24,4 +24,5 @@
 ###
 
 # Start Clamp backend in DEBUG mode
+mkdir -p /var/log/onap
 java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9999 -jar ../../target/clamp.jar --spring.config.name=application-noaaf
