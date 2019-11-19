onap/clamp

Adding View BP template Menu
Adding View Blue Print templates option under Template menu. Adding few
more test cases for ViewTosca Modals to cover the api calls.

Issue-ID: CLAMP-544
Change-Id: Ibf881f46fd03e9149a88ccc211a996d99fd74748
Signed-off-by: drveerendra <vrajasekharaiah@att.com>

ui-react/src/LoopUI.js
@@ -42,7 +42,8 @@ import ConfigurationPolicyModal from './components/dialogs/ConfigurationPolicy/C
 import LoopProperties from './components/dialogs/LoopProperties';
 import UserInfo from './components/dialogs/UserInfo';
 import LoopService from './api/LoopService';
-import ViewToscaModal from './components/dialogs/ViewToscaModal/ViewToscaModal';
+import ViewToscaModals from './components/dialogs/ViewToscaModals/ViewToscaModals';
+import ViewBPTempls from './components/dialogs/ViewBPTempls/ViewBPTempls';
 import PerformAction from './components/dialogs/PerformActions';
 import RefreshStatus from './components/dialogs/RefreshStatus';
 import DeployLoop from './components/dialogs/DeployLoop';
@@ -248,7 +249,8 @@ export default class LoopUI extends React.Component {
 	render() {
 		return (
 				<StyledMainDiv id="main_div">
-				<Route path="/viewToscaModal" render={(routeProps) => (<ViewToscaModal {...routeProps} />)} />
+				<Route path="/viewToscaModals" render={(routeProps) => (<ViewToscaModals {...routeProps} />)} />
+				<Route path="/viewBPTempls" render={(routeProps) => (<ViewBPTempls {...routeProps} />)} />
 				<Route path="/operationalPolicyModal"
 					render={(routeProps) => (<OperationalPolicyModal {...routeProps} loopCache={this.getLoopCache()} loadLoopFunction={this.loadLoop} showAlert={this.showAlert}/>)} />
 				<Route path="/configurationPolicyModal/:componentName" render={(routeProps) => (<ConfigurationPolicyModal {...routeProps} loopCache={this.getLoopCache()} loadLoopFunction={this.loadLoop}/>)} />

ui-react/src/api/TemplateMenuService.js
@@ -37,4 +37,21 @@ export default class TemplateMenuService {
         return {};
       });
   }
+  
+  static getBPTempls() {
+	    return fetch('restservices/clds/v2/loop/getAllLoopTemplates', { method: 'GET', credentials: 'same-origin', })
+	      .then(function (response) {
+	        console.debug("getBPTempls response received: ", response.status);
+	        if (response.ok) {
+	          return response.json();
+	        } else {
+	          console.error("getBPTempl squery failed");
+	          return {};
+	        }
+	      })
+	      .catch(function (error) {
+	        console.error("getBPTempls error received", error);
+	        return {};
+	      });
+	  }
 }

ui-react/src/components/menu/MenuBar.js
@@ -88,7 +88,8 @@ export default class MenuBar extends React.Component {
 
 				<Navbar.Collapse>
 					<StyledNavDropdown title="Template">
-							<NavDropdown.Item as={StyledLink} to="/viewToscaModal">View Tosca Model</NavDropdown.Item>
+							<NavDropdown.Item as={StyledLink} to="/viewToscaModals">View Tosca Model</NavDropdown.Item>
+							<NavDropdown.Item as={StyledLink} to="/viewBPTempls">View Blue Print Templates</NavDropdown.Item>
 					</StyledNavDropdown>
 					<StyledNavDropdown title="Closed Loop">
 							<NavDropdown.Item as={StyledLink} to="/openLoop">Open CL</NavDropdown.Item>

onap/policy-clamp

ui-react/src/LoopUI.js
@@ -42,7 +42,8 @@ import ConfigurationPolicyModal from './components/dialogs/ConfigurationPolicy/C
 import LoopProperties from './components/dialogs/LoopProperties';
 import UserInfo from './components/dialogs/UserInfo';
 import LoopService from './api/LoopService';
-import ViewToscaModal from './components/dialogs/ViewToscaModal/ViewToscaModal';
+import ViewToscaPolicyModal from './components/dialogs/Tosca/ViewToscaPolicyModal';
+import ViewBlueprintMicroServiceTemplatesModal from './components/dialogs/Tosca/ViewBlueprintMicroServiceTemplatesModal';
 import PerformAction from './components/dialogs/PerformActions';
 import RefreshStatus from './components/dialogs/RefreshStatus';
 import DeployLoop from './components/dialogs/DeployLoop';
@@ -248,7 +249,8 @@ export default class LoopUI extends React.Component {
 	render() {
 		return (
 				<StyledMainDiv id="main_div">
-				<Route path="/viewToscaModal" render={(routeProps) => (<ViewToscaModal {...routeProps} />)} />
+				<Route path="/viewToscaPolicyModal" render={(routeProps) => (<ViewToscaPolicyModal {...routeProps} />)} />
+				<Route path="/viewBlueprintMicroServiceTemplatesModal" render={(routeProps) => (<ViewBlueprintMicroServiceTemplatesModal {...routeProps} />)} />
 				<Route path="/operationalPolicyModal"
 					render={(routeProps) => (<OperationalPolicyModal {...routeProps} loopCache={this.getLoopCache()} loadLoopFunction={this.loadLoop} showAlert={this.showAlert}/>)} />
 				<Route path="/configurationPolicyModal/:componentName" render={(routeProps) => (<ConfigurationPolicyModal {...routeProps} loopCache={this.getLoopCache()} loadLoopFunction={this.loadLoop}/>)} />

ui-react/src/api/TemplateMenuService.js
@@ -21,19 +21,36 @@
  */
 
 export default class TemplateMenuService {
-  static getToscaModels() {
+  static getToscaPolicyModels() {
     return fetch('restservices/clds/v2/loop/tosca/models', { method: 'GET', credentials: 'same-origin' })
       .then(function (response) {
-        console.debug("getToscaModels response received: ", response.status);
+        console.debug("getToscaPolicyModels response received: ", response.status);
         if (response.ok) {
           return response.json();
         } else {
-          console.error("getToscaModels query failed");
+          console.error("getToscaPolicyModels query failed");
           return {};
         }
       })
       .catch(function (error) {
-        console.error("getToscaModels error received", error);
+        console.error("getToscaPolicyModels error received", error);
+        return {};
+      });
+  }
+
+  static getBlueprintMicroServiceTemplates() {
+    return fetch('restservices/clds/v2/loop/templates', { method: 'GET', credentials: 'same-origin', })
+      .then(function (response) {
+        console.debug("getBlueprintMicroServiceTemplates response received: ", response.status);
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.error("getBlueprintMicroServiceTemplates query failed");
+          return {};
+        }
+      })
+      .catch(function (error) {
+        console.error("getBlueprintMicroServiceTemplates error received", error);
         return {};
       });
   }

ui-react/src/components/menu/MenuBar.js
@@ -88,7 +88,8 @@ export default class MenuBar extends React.Component {
 
 				<Navbar.Collapse>
 					<StyledNavDropdown title="Template">
-							<NavDropdown.Item as={StyledLink} to="/viewToscaModal">View Tosca Model</NavDropdown.Item>
+							<NavDropdown.Item as={StyledLink} to="/viewToscaPolicyModal">View Tosca Policy Models</NavDropdown.Item>
+							<NavDropdown.Item as={StyledLink} to="/viewBlueprintMicroServiceTemplatesModal">View Blueprint MicroService Templates</NavDropdown.Item>
 					</StyledNavDropdown>
 					<StyledNavDropdown title="Closed Loop">
 							<NavDropdown.Item as={StyledLink} to="/openLoop">Open CL</NavDropdown.Item>