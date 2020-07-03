onap/clamp

add robotframework to integration tests
Issue-ID: CLAMP-844
Signed-off-by: JulienBe <julien.bertozzi@intl.att.com>
Change-Id: Ie5c40477f42fcf26f5fef6c1742e34a2eee7f73d

src/test/resources/robotframework/tests/01_healthcheck.robot
@@ -1,0 +1,22 @@
+*** Settings ***
+Library     Collections
+Library     RequestsLibrary
+Library     OperatingSystem
+Library     json
+Library     OperatingSystem
+
+*** Variables ***
+${login}                     admin
+${passw}                     password
+
+*** Keywords ***
+Create the sessions
+
+*** Test Cases ***
+Get Requests health check ok
+    ${port} =    Get Environment Variable   CLAMP_PORT
+    ${auth}=    Create List     ${login}    ${passw}
+    Create Session   clamp  https://localhost:${port}   auth=${auth}   disable_warnings=1
+    Set Global Variable     ${clamp_session}      clamp
+    ${resp}=    Get Request    ${clamp_session}   /restservices/clds/v1/healthcheck
+    Should Be Equal As Strings  ${resp.status_code}     200

onap/policy-clamp

src/test/resources/robotframework/tests/01_healthcheck.robot
@@ -1,0 +1,19 @@
+*** Settings ***
+Library     Collections
+Library     RequestsLibrary
+Library     OperatingSystem
+Library     json
+Library     OperatingSystem
+*** Variables ***
+${login}                     admin
+${passw}                     password
+*** Keywords ***
+Create the sessions
+*** Test Cases ***
+Get Requests health check ok
+    ${port} =    Get Environment Variable   CLAMP_PORT
+    ${auth}=    Create List     ${login}    ${passw}
+    Create Session   clamp  http://localhost:${port}   auth=${auth}   disable_warnings=1
+    Set Global Variable     ${clamp_session}      clamp
+    ${resp}=    Get Request    ${clamp_session}   /restservices/clds/v1/healthcheck
+    Should Be Equal As Strings  ${resp.status_code}     200