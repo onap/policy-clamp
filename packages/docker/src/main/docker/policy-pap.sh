1b5c90a6a76524571bc093055c4e098a9a69580f
liamfallon <liam.fallon@est.tech>
2021-01-12 12:35:11 +0000

    Policy Modular strucuture to TOSCA POC repo
    We are basing the TOSCA POC code on the policy PAP structure and it
    makes it easier for us to have a modular repo structure for the TOSCA
    POC similar to the other Policy repos, especially PAP.
    
    In this review, which is only for the TOSCA PoC, we have created a
    modular strucuture, with two main code modules, one for the current
    CLAMP code (clds) and one for the control loop (controlloop). The other
    directories at root level are the standard policy ones of
    packages/postman/releases/testsuites.
    
    The content of the "clds" directory is identical to the existing CLAMP
    code in all respects except for the src/main/script/checkLibINdex.sh
    script, where I had to modify the values of three path variables at the
    top to get it to work in the "clds" subdirectory.
    
    The module structure is:
    ./clds                (maven module, existing CLAMP)
    ./clds/docs
    ./clds/extra
    ./clds/src
    ./clds/ui-react
    ./clds/ui-react-lib
    ./controlloop         (maven module)
    ./controlloop/common  (maven module, could be moved to policy-common)
    ./controlloop/design  (maven module, control loop design time component)
    ./controlloop/models  (maven module, should be moved to policy-models)
    ./controlloop/participants (maven modue for control loop participants)
    ./controlloop/runtime (maven module, control loop run time component)
    ./packages            (maven module, standard PF packaging))
    ./packages/docker     (maven module, standard PF docker support)
    ./packages/tarball    (maven module, standard PF tarball generation)
    ./postman             (maven module, standard PF postman collections)
    ./releases
    ./testsuites             (maven module, standard PF testsuite structure)
    ./testsuites/performance (maven module, standard PF performance tests)
    ./testsuites/stability   (maven module, standard PF stability tests)
    
    Issue-ID: POLICY-2971
    Change-Id: I98c7e25f592cc7d9783d5daff3e9e3ad2e7b5214
    Signed-off-by: liamfallon <liam.fallon@est.tech>

---
+++ packages/docker/src/main/docker/policy-pap.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env ash
+#
+# ============LICENSE_START=======================================================
+#  Copyright (C) 2019-2020 Nordix Foundation.
+#  Modifications Copyright (C) 2019-2020 AT&T Intellectual Property.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# ============LICENSE_END=========================================================
+#
+
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk/
+KEYSTORE="${KEYSTORE:-$POLICY_HOME/etc/ssl/policy-keystore}"
+TRUSTSTORE="${TRUSTSTORE:-$POLICY_HOME/etc/ssl/policy-truststore}"
+KEYSTORE_PASSWD="${KEYSTORE_PASSWD:-Pol1cy_0nap}"
+TRUSTSTORE_PASSWD="${TRUSTSTORE_PASSWD:-Pol1cy_0nap}"
+
+if [ "$#" -ge 1 ]; then
+    CONFIG_FILE=$1
+else
+    CONFIG_FILE=${CONFIG_FILE}
+fi
+
+if [ -z "$CONFIG_FILE" ]; then
+    CONFIG_FILE="${POLICY_HOME}/etc/defaultConfig.json"
+fi
+
+echo "Policy pap config file: $CONFIG_FILE"
+
+if [ -f "${POLICY_HOME}/etc/mounted/policy-truststore" ]; then
+    echo "overriding policy-truststore"
+    cp -f "${POLICY_HOME}"/etc/mounted/policy-truststore "${TRUSTSTORE}"
+fi
+
+if [ -f "${POLICY_HOME}/etc/mounted/policy-keystore" ]; then
+    echo "overriding policy-keystore"
+    cp -f "${POLICY_HOME}"/etc/mounted/policy-keystore "${KEYSTORE}"
+fi
+
+if [ -f "${POLICY_HOME}/etc/mounted/logback.xml" ]; then
+    echo "overriding logback.xml"
+    cp -f "${POLICY_HOME}"/etc/mounted/logback.xml "${POLICY_HOME}"/etc/
+fi
+
+$JAVA_HOME/bin/java -cp "${POLICY_HOME}/etc:${POLICY_HOME}/lib/*" -Dlogback.configurationFile="${POLICY_HOME}/etc/logback.xml" -Djavax.net.ssl.keyStore="${KEYSTORE}" -Djavax.net.ssl.keyStorePassword="${KEYSTORE_PASSWD}" -Djavax.net.ssl.trustStore="${TRUSTSTORE}" -Djavax.net.ssl.trustStorePassword="${TRUSTSTORE_PASSWD}" org.onap.policy.pap.main.startstop.Main -c "${CONFIG_FILE}"
