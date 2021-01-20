49b035a44b4ab83806dd87dfcd05c4f255db1645
sebdet <sebastien.determe@intl.att.com>
2021-01-20 23:02:17 +0100

    First integration of Policy model PdpGroups
    Replacement of the PdpGroups clamp code by the one from policy model dependency.
    
    Issue-ID: POLICY-2924
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>
    Change-Id: I12c77704bba1c769dc2f307d91b759a3b645013f

---
+++ src/main/java/org/onap/policy/clamp/policy/pdpgroup/PdpGroupsAnalyzer.java
@@ -0,0 +1,98 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2021 AT&T Intellectual Property. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.policy.clamp.policy.pdpgroup;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.util.List;
+import org.onap.policy.clamp.loop.template.PolicyModel;
+import org.onap.policy.models.pdp.concepts.PdpGroup;
+import org.onap.policy.models.pdp.concepts.PdpGroups;
+import org.onap.policy.models.pdp.concepts.PdpSubGroup;
+import org.onap.policy.models.pdp.enums.PdpState;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaPolicyTypeIdentifier;
+import org.onap.sdc.toscaparser.api.elements.PolicyType;
+
+/**
+ * This is an utility class to do searching in pdp groups.
+ */
+public class PdpGroupsAnalyzer {
+    /**
+     * Get supported subGroups based on the defined policy type and version for s specific PDPgroup.
+     * It returns null if the Group is not ACTIVE or if the policytype/version has not been found in the PDPSubgroups.
+     *
+     * @param pdpGroup   The pdpGroup that must be analyzed
+     * @param policyType The policy type
+     * @param version    The version
+     * @return The supported subGroups list in Json format
+     * @see org.onap.policy.models.pdp.concepts.PdpGroup
+     * @see org.onap.policy.models.pdp.enums.PdpState
+     */
+    static private JsonObject getSupportedSubgroups(PdpGroup pdpGroup, String policyType, String version) {
+        if (!PdpState.ACTIVE.equals(pdpGroup.getPdpGroupState())) {
+            return null;
+        }
+        JsonArray supportedSubgroups = new JsonArray();
+        for (PdpSubGroup subGroup : pdpGroup.getPdpSubgroups()) {
+            for (ToscaPolicyTypeIdentifier policyTypeIdentifier:subGroup.getSupportedPolicyTypes()) {
+                if (policyType.matches(policyTypeIdentifier.getName().replace(".","\\.").replace("*",".*")) && version.equals(policyTypeIdentifier.getVersion())) {
+                    supportedSubgroups.add(subGroup.getPdpType());
+                    break;
+                }
+            }
+        }
+        if (supportedSubgroups.size() > 0) {
+            JsonObject supportedPdpGroup = new JsonObject();
+            supportedPdpGroup.add(pdpGroup.getName(), supportedSubgroups);
+            return supportedPdpGroup;
+        }
+        return null;
+    }
+
+    /**
+     * This method updates each element in the policyModelsList given in argument based on the pdpGroups given.
+     * @param policyModelsList The list of Policy Models where each PolicyModel will be updated
+     * @param pdpGroups The PdpGroups containing all PDP group definition
+     */
+    static public void updatePdpGroup(List<PolicyModel> policyModelsList, PdpGroups pdpGroups) {
+        for (PolicyModel policyModel : policyModelsList) {
+            JsonArray supportedPdpGroups = new JsonArray();
+            for (PdpGroup pdpGroup : pdpGroups.getGroups()) {
+                JsonObject supportedPdpGroup = PdpGroupsAnalyzer.getSupportedSubgroups(pdpGroup,
+                        policyModel.getPolicyModelType(), policyModel.getVersion());
+                if (supportedPdpGroup != null) {
+                    supportedPdpGroups.add(supportedPdpGroup);
+                }
+            }
+
+            if (supportedPdpGroups.size() > 0) {
+                JsonObject supportedPdpJson = new JsonObject();
+                supportedPdpJson.add("supportedPdpGroups", supportedPdpGroups);
+                policyModel.setPolicyPdpGroup(supportedPdpJson);
+            } else {
+                policyModel.setPolicyPdpGroup(null);
+            }
+        }
+    }
+}
