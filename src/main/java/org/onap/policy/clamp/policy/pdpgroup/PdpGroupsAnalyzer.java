6939105238ba2009f1fe3cb9cd912c4650295a28
sebdet <sebastien.determe@intl.att.com>
2021-01-22 17:26:04 +0100

    Policy list and Pdpgroup merger code
    Draft code of the policy list/pdpgroup merger + tests, this code will most likely be called by a camel Rest route and the answer will be returned to the client/ui. The policies listing is enriched with new fields pdp group info, like:
      1. In which Pdp group/Subgroup the policy is currently deployed
      2. What are the potential pdpgroups/subgroups where the policy can be deployed
    
      These info will be shown in the policy clamp UI to the user.
    
    Also some refactoring has been done for the sake of clarity.
    
    Issue-ID: POLICY-2924
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>
    Change-Id: Iba1ab6fea90735551d7d2e2bbff9ab2efdee093e
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>
    (cherry picked from commit 76a0b386bcf5f30533c85f6d26c559bea19ce0fb)

--- src/main/java/org/onap/policy/clamp/policy/pdpgroup/PdpGroupsAnalyzer.java
+++ src/main/java/org/onap/policy/clamp/policy/pdpgroup/PdpGroupsAnalyzer.java
@@ -1,6 +1,6 @@
 /*-
  * ============LICENSE_START=======================================================
- * ONAP CLAMP
+ * ONAP POLICY-CLAMP
  * ================================================================================
  * Copyright (C) 2021 AT&T Intellectual Property. All rights
  *                             reserved.
@@ -27,19 +27,19 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.Optional;
 import org.onap.policy.clamp.loop.template.PolicyModel;
 import org.onap.policy.models.pdp.concepts.PdpGroup;
 import org.onap.policy.models.pdp.concepts.PdpGroups;
 import org.onap.policy.models.pdp.concepts.PdpSubGroup;
 import org.onap.policy.models.pdp.enums.PdpState;
-import org.onap.policy.models.tosca.authorative.concepts.ToscaPolicyTypeIdentifier;
-import org.onap.sdc.toscaparser.api.elements.PolicyType;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaPolicyIdentifier;
 
 /**
  * This is an utility class to do searching in pdp groups.
  */
 public class PdpGroupsAnalyzer {
+
     /**
      * Get supported subGroups based on the defined policy type and version for s specific PDPgroup.
      * It returns null if the Group is not ACTIVE or if the policytype/version has not been found in the PDPSubgroups.
@@ -51,7 +51,8 @@ public class PdpGroupsAnalyzer {
      * @see org.onap.policy.models.pdp.concepts.PdpGroup
      * @see org.onap.policy.models.pdp.enums.PdpState
      */
-    private static JsonObject getSupportedSubgroups(PdpGroup pdpGroup, String policyType, String version) {
+    private static JsonObject getSupportedPdpSubgroupsForModelType(PdpGroup pdpGroup, String policyType,
+                                                                   String version) {
         if (!PdpState.ACTIVE.equals(pdpGroup.getPdpGroupState())) {
             return null;
         }
@@ -65,10 +66,27 @@ public class PdpGroupsAnalyzer {
                 supportedSubgroups.add(pdpSubGroup.getPdpType());
             }
         });
-        if (supportedSubgroups.size() == 0) {
-            return null;
-        }
-        return supportedPdpGroup;
+        return supportedSubgroups.size() == 0 ? null : supportedPdpGroup;
+    }
+
+    /**
+     * This method retrieves all supported pdpGroups and subgroups for a specific policy type/version.
+     *
+     * @param pdpGroups  The PdpGroups object containing all PEF pdp groups info
+     * @param policyType The policy type that must be used for searching
+     * @param version    THe policy type version that must be used for searching
+     * @return It returns a JsonObject containing each pdpGroup and subgroups associated
+     */
+    public static JsonObject getSupportedPdpGroupsForModelType(PdpGroups pdpGroups, String policyType, String version) {
+        JsonObject supportedPdpGroups = new JsonObject();
+        JsonArray pdpGroupsArray = new JsonArray();
+        supportedPdpGroups.add("supportedPdpGroups", pdpGroupsArray);
+
+        pdpGroups.getGroups().stream().map(pdpGroup -> PdpGroupsAnalyzer.getSupportedPdpSubgroupsForModelType(pdpGroup,
+                policyType, version)).filter(Objects::nonNull)
+                .forEach(jsonPdpGroup -> pdpGroupsArray.add(jsonPdpGroup));
+
+        return pdpGroupsArray.size() != 0 ? supportedPdpGroups : null;
     }
 
     /**
@@ -77,18 +95,39 @@ public class PdpGroupsAnalyzer {
      * @param policyModelsList The list of Policy Models where each PolicyModel will be updated
      * @param pdpGroups        The PdpGroups containing all PDP group definition
      */
-    public static void updatePdpGroup(List<PolicyModel> policyModelsList, PdpGroups pdpGroups) {
+    public static void updatePdpGroupOfPolicyModels(List<PolicyModel> policyModelsList, PdpGroups pdpGroups) {
         policyModelsList.parallelStream().forEach(policyModel -> {
-            JsonObject jsonResult = new JsonObject();
-            JsonArray supportedPdpGroups = new JsonArray();
-            jsonResult.add("supportedPdpGroups", supportedPdpGroups);
-            policyModel.setPolicyPdpGroup(jsonResult);
-            pdpGroups.getGroups().stream().map(pdpGroup -> PdpGroupsAnalyzer.getSupportedSubgroups(pdpGroup,
-                    policyModel.getPolicyModelType(), policyModel.getVersion())).filter(Objects::nonNull)
-                    .forEach(jsonPdpGroup -> supportedPdpGroups.add(jsonPdpGroup));
-            if (supportedPdpGroups.size() == 0) {
-                policyModel.setPolicyPdpGroup(null);
-            }
+            policyModel.setPolicyPdpGroup(getSupportedPdpGroupsForModelType(pdpGroups, policyModel.getPolicyModelType(),
+                    policyModel.getVersion()));
         });
+    }
+
+    /**
+     * This method searches for the PdpGroup/subgroup where the policy given is currently deployed.
+     *
+     * @param pdpGroups The pdpGroups info from PEF
+     * @param policyName The policy Id
+     * @param version The policy version
+     * @return It returns a JsonObject containing the pdpGroup/subgroup info
+     */
+    public static JsonObject getPdpGroupDeploymentOfOnePolicy(PdpGroups pdpGroups, String policyName, String version) {
+        JsonObject pdpGroupInfo = new JsonObject();
+        JsonObject assignedPdpGroups = new JsonObject();
+        pdpGroupInfo.add("pdpGroupInfo", assignedPdpGroups);
+
+        ToscaPolicyIdentifier toscaPolicyIdentifier = new ToscaPolicyIdentifier(policyName, version);
+        pdpGroups.getGroups().stream().anyMatch(pdpGroup ->
+                pdpGroup.getPdpSubgroups().stream().anyMatch(
+                        pdpSubGroup -> {
+                            if (pdpSubGroup.getPolicies() != null && pdpSubGroup.getPolicies()
+                                    .contains(toscaPolicyIdentifier)) {
+                                assignedPdpGroups.addProperty("pdpGroup", pdpGroup.getName());
+                                assignedPdpGroups.addProperty("pdpSubGroup", pdpSubGroup.getPdpType());
+                                return true;
+                            }
+                            return false;
+                        })
+        );
+        return assignedPdpGroups.entrySet().isEmpty() ? null : pdpGroupInfo;
     }
 }
