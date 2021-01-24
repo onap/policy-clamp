eb8e3f1dd891ac98f1bbaf35a2a0679146785236
sebdet <sebastien.determe@intl.att.com>
2021-01-24 18:12:36 +0100

    Add New UI component for policies list
    Add new Ui components to list policies + the small refactoring of React Routes
    
    Issue-ID: POLICY-2925
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>
    Change-Id: I784d7c144a3a3af98a9d62b5d40b5172dbdaed47
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- ui-react/src/components/menu/MenuBar.js
+++ ui-react/src/components/menu/MenuBar.js
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * ONAP CLAMP
  * ================================================================================
- * Copyright (C) 2019 AT&T Intellectual Property. All rights
+ * Copyright (C) 2019, 2021 AT&T Intellectual Property. All rights
  *                             reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,94 +30,95 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const StyledLink = styled(Link)`
-	color: ${props => props.theme.menuFontColor};
-	background-color: ${props => props.theme.menuBackgroundColor};
-	font-weight: normal;
-	display: block;
-	width: 100%;
-	padding: .25rem 1.5rem;
-	clear: both;
-	text-align: inherit;
-	white-space: nowrap;
-	border: 0;
-	:hover {
-		text-decoration: none;
-		background-color: ${props => props.theme.menuHighlightedBackgroundColor};
-		color:  ${props => props.theme.menuHighlightedFontColor};
-	}
+    color: ${props => props.theme.menuFontColor};
+    background-color: ${props => props.theme.menuBackgroundColor};
+    font-weight: normal;
+    display: block;
+    width: 100%;
+    padding: .25rem 1.5rem;
+    clear: both;
+    text-align: inherit;
+    white-space: nowrap;
+    border: 0;
+    :hover {
+        text-decoration: none;
+        background-color: ${props => props.theme.menuHighlightedBackgroundColor};
+        color:  ${props => props.theme.menuHighlightedFontColor};
+    }
 `;
 const StyledNavLink = styled(Nav.Link)`
-	color: ${props => props.theme.menuFontColor};
-	background-color: ${props => props.theme.menuBackgroundColor};
-	font-weight: normal;
-	padding: .25rem 1.5rem;
-	:hover {
-		background-color: ${props => props.theme.menuHighlightedBackgroundColor};
-		color:  ${props => props.theme.menuHighlightedFontColor};
-	}
+    color: ${props => props.theme.menuFontColor};
+    background-color: ${props => props.theme.menuBackgroundColor};
+    font-weight: normal;
+    padding: .25rem 1.5rem;
+    :hover {
+        background-color: ${props => props.theme.menuHighlightedBackgroundColor};
+        color:  ${props => props.theme.menuHighlightedFontColor};
+    }
 `;
 
 const StyledNavDropdown = styled(NavDropdown)`
-	color: ${props => props.theme.menuFontColor};
-	& .dropdown-toggle {
-	color: ${props => props.theme.menuFontColor};
-	background-color: ${props => props.theme.backgroundColor};
-	font-weight: normal;
-		:hover {
-				font-weight: bold;
-		}
-	}
+    color: ${props => props.theme.menuFontColor};
+    & .dropdown-toggle {
+    color: ${props => props.theme.menuFontColor};
+    background-color: ${props => props.theme.backgroundColor};
+    font-weight: normal;
+        :hover {
+                font-weight: bold;
+        }
+    }
 `;
 
 export default class MenuBar extends React.Component {
-	state = {
-		loopName: this.props.loopName,
-		disabled: true
-	};
+    state = {
+        loopName: this.props.loopName,
+        disabled: true
+    };
 
-	componentWillReceiveProps(newProps) {
-		if (newProps.loopName !== OnapConstants.defaultLoopName) {
-			this.setState({ disabled: false });
-		} else {
-			this.setState({ disabled: true });
-		}
-	}
+    componentWillReceiveProps(newProps) {
+        if (newProps.loopName !== OnapConstants.defaultLoopName) {
+            this.setState({ disabled: false });
+        } else {
+            this.setState({ disabled: true });
+        }
+    }
 
-	render () {
-		return (
-				<Navbar.Collapse>
-					<StyledNavDropdown title="Loop Templates">
-							<NavDropdown.Item as={StyledLink} to="/ViewLoopTemplatesModal">View All Templates</NavDropdown.Item>
-					</StyledNavDropdown>
-					<StyledNavDropdown title="Policy Models">
-                    	    <NavDropdown.Item as={StyledLink} to="/uploadToscaPolicyModal">Upload Tosca Model</NavDropdown.Item>
-                    		<NavDropdown.Item as={StyledLink} to="/viewToscaPolicyModal">View Tosca Models</NavDropdown.Item>
-                            <NavDropdown.Item as={StyledLink} to="/ManageDictionaries">Manage Metadata Dictionaries</NavDropdown.Item>
+    render () {
+        return (
+                <Navbar.Collapse>
+                    <StyledNavDropdown title="Loop Templates">
+                            <NavDropdown.Item as={StyledLink} to="/viewLoopTemplatesModal">View All Templates</NavDropdown.Item>
                     </StyledNavDropdown>
-					<StyledNavDropdown title="Loop Instance">
-					        <NavDropdown.Item as={StyledLink} to="/createLoop">Create</NavDropdown.Item>
-					        <NavDropdown.Item as={StyledLink} to="/openLoop">Open</NavDropdown.Item>
-					        <NavDropdown.Item as={StyledLink} to="/closeLoop" disabled={this.state.disabled}>Close</NavDropdown.Item>
-					        <NavDropdown.Item as={StyledLink} to="/modifyLoop" disabled={this.state.disabled}>Modify</NavDropdown.Item>
-						    <NavDropdown.Divider />
-							<NavDropdown.Item as={StyledLink} to="/loopProperties" disabled={this.state.disabled}>Properties</NavDropdown.Item>
-							<NavDropdown.Item as={StyledLink} to="/refreshStatus" disabled={this.state.disabled}>Refresh Status</NavDropdown.Item>
-					</StyledNavDropdown>
-					<StyledNavDropdown title="Loop Operations">
-							<NavDropdown.Item as={StyledLink} to="/submit" disabled={this.state.disabled}>Create and deploy to Policy Engine(SUBMIT)</NavDropdown.Item>
-							<NavDropdown.Item as={StyledLink} to="/stop" disabled={this.state.disabled}>Undeploy from Policy Engine (STOP)</NavDropdown.Item>
-							<NavDropdown.Item as={StyledLink} to="/restart" disabled={this.state.disabled}>ReDeploy to Policy Engine (RESTART)</NavDropdown.Item>
-							<NavDropdown.Item as={StyledLink} to="/delete" disabled={this.state.disabled}>Delete loop instance (DELETE)</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item as={StyledLink} to="/deploy" disabled={this.state.disabled}>Deploy to DCAE (DEPLOY)</NavDropdown.Item>
-							<NavDropdown.Item as={StyledLink} to="/undeploy" disabled={this.state.disabled}>UnDeploy to DCAE (UNDEPLOY)</NavDropdown.Item>
-					</StyledNavDropdown>
-					<StyledNavDropdown title="Help">
-							<StyledNavLink href="https://wiki.onap.org/" target="_blank">Wiki</StyledNavLink>
-							<StyledNavLink href="mailto:onap-discuss@lists.onap.org?subject=CLAMP&body=Please send us suggestions or feature enhancements or defect. If possible, please send us the steps to replicate any defect.">Contact Us</StyledNavLink>
-							<NavDropdown.Item as={StyledLink} to="/userInfo">User Info</NavDropdown.Item>
-					</StyledNavDropdown>
-				</Navbar.Collapse>
-		);
-	}
+                    <StyledNavDropdown title="Policy Models">
+                            <NavDropdown.Item as={StyledLink} to="/uploadToscaPolicyModal">Upload Tosca Model</NavDropdown.Item>
+                            <NavDropdown.Item as={StyledLink} to="/viewToscaPolicyModal">View Tosca Models</NavDropdown.Item>
+                            <NavDropdown.Item as={StyledLink} to="/manageDictionaries">Manage Metadata Dictionaries</NavDropdown.Item>
+                            <NavDropdown.Item as={StyledLink} to="/viewAllPolicies">View All Policies</NavDropdown.Item>
+                    </StyledNavDropdown>
+                    <StyledNavDropdown title="Loop Instance">
+                            <NavDropdown.Item as={StyledLink} to="/createLoop">Create</NavDropdown.Item>
+                            <NavDropdown.Item as={StyledLink} to="/openLoop">Open</NavDropdown.Item>
+                            <NavDropdown.Item as={StyledLink} to="/closeLoop" disabled={this.state.disabled}>Close</NavDropdown.Item>
+                            <NavDropdown.Item as={StyledLink} to="/modifyLoop" disabled={this.state.disabled}>Modify</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item as={StyledLink} to="/loopProperties" disabled={this.state.disabled}>Properties</NavDropdown.Item>
+                            <NavDropdown.Item as={StyledLink} to="/refreshStatus" disabled={this.state.disabled}>Refresh Status</NavDropdown.Item>
+                    </StyledNavDropdown>
+                    <StyledNavDropdown title="Loop Operations">
+                            <NavDropdown.Item as={StyledLink} to="/submit" disabled={this.state.disabled}>Create and deploy to Policy Engine(SUBMIT)</NavDropdown.Item>
+                            <NavDropdown.Item as={StyledLink} to="/stop" disabled={this.state.disabled}>Undeploy from Policy Engine (STOP)</NavDropdown.Item>
+                            <NavDropdown.Item as={StyledLink} to="/restart" disabled={this.state.disabled}>ReDeploy to Policy Engine (RESTART)</NavDropdown.Item>
+                            <NavDropdown.Item as={StyledLink} to="/delete" disabled={this.state.disabled}>Delete loop instance (DELETE)</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item as={StyledLink} to="/deploy" disabled={this.state.disabled}>Deploy to DCAE (DEPLOY)</NavDropdown.Item>
+                            <NavDropdown.Item as={StyledLink} to="/undeploy" disabled={this.state.disabled}>UnDeploy to DCAE (UNDEPLOY)</NavDropdown.Item>
+                    </StyledNavDropdown>
+                    <StyledNavDropdown title="Help">
+                            <StyledNavLink href="https://wiki.onap.org/" target="_blank">Wiki</StyledNavLink>
+                            <StyledNavLink href="mailto:onap-discuss@lists.onap.org?subject=CLAMP&body=Please send us suggestions or feature enhancements or defect. If possible, please send us the steps to replicate any defect.">Contact Us</StyledNavLink>
+                            <NavDropdown.Item as={StyledLink} to="/userInfo">User Info</NavDropdown.Item>
+                    </StyledNavDropdown>
+                </Navbar.Collapse>
+        );
+    }
 }
