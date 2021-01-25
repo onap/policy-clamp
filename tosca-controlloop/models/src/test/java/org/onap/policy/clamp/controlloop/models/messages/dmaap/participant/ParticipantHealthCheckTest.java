f59d4d655f7b7218b8ec9578521703ae0f17cb6d
liamfallon <liam.fallon@est.tech>
2021-01-25 16:23:28 +0000

    Add common and models initial code
    This review brings inthe initial common and models code for the TOSCA
    control loop PoC
    
    Changes are made to the model that were required to implement the
    working demo.
    
    Unit test are now added that brings coverage above 90%
    
    Issue-ID: POLICY-2995
    Change-Id: I22e484a6e6195b2b74087b62fb3bc51d541b171e
    Signed-off-by: liamfallon <liam.fallon@est.tech>

---
+++ tosca-controlloop/models/src/test/java/org/onap/policy/clamp/controlloop/models/messages/dmaap/participant/ParticipantHealthCheckTest.java
@@ -0,0 +1,60 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2021 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.clamp.controlloop.models.messages.dmaap.participant;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.Assert.assertEquals;
+import static org.onap.policy.clamp.controlloop.models.messages.dmaap.participant.ParticipantMessageUtils.removeVariableFields;
+
+import java.util.UUID;
+import org.junit.Test;
+import org.onap.policy.clamp.controlloop.models.controlloop.concepts.ParticipantState;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaConceptIdentifier;
+
+/**
+ * Test the copy constructor and other methods.
+ */
+public class ParticipantHealthCheckTest {
+
+    @Test
+    public void testCopyConstructor() {
+        assertThatThrownBy(() -> new ParticipantStateChange(null)).isInstanceOf(NullPointerException.class);
+
+        ParticipantHealthCheck orig = new ParticipantHealthCheck();
+
+        // verify with null values
+        assertEquals(removeVariableFields(orig.toString()),
+                removeVariableFields(new ParticipantHealthCheck(orig).toString()));
+
+        // verify with all values
+        ToscaConceptIdentifier id = new ToscaConceptIdentifier();
+        id.setName("id");
+        id.setVersion("1.2.3");
+        orig.setControlLoopId(id);
+        orig.setParticipantId(id);
+        orig.setRequestId(UUID.randomUUID());
+        orig.setState(ParticipantState.ACTIVE);
+        orig.setTimestampMs(Long.valueOf(3000));
+
+        assertEquals(removeVariableFields(orig.toString()),
+                        removeVariableFields(new ParticipantHealthCheck(orig).toString()));
+    }
+}
---
+++ tosca-controlloop/models/src/test/java/org/onap/policy/clamp/controlloop/models/messages/dmaap/participant/ParticipantMessageTest.java
@@ -0,0 +1,101 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2021 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.clamp.controlloop.models.messages.dmaap.participant;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.UUID;
+import org.junit.Test;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaConceptIdentifier;
+
+public class ParticipantMessageTest {
+    private ParticipantMessage message;
+
+    @Test
+    public void testCopyConstructor() {
+        assertThatThrownBy(() -> new ParticipantMessage((ParticipantMessage) null))
+                .isInstanceOf(NullPointerException.class);
+
+        // verify with null values
+        message = new ParticipantMessage(ParticipantMessageType.PARTICIPANT_STATE_CHANGE);
+        ParticipantMessage newmsg = new ParticipantMessage(message);
+        newmsg.setRequestId(message.getRequestId());
+        newmsg.setTimestampMs(message.getTimestampMs());
+        assertEquals(message.toString(), newmsg.toString());
+
+        // verify with all values
+        message = makeMessage();
+        newmsg = new ParticipantMessage(message);
+        newmsg.setRequestId(message.getRequestId());
+        newmsg.setTimestampMs(message.getTimestampMs());
+        assertEquals(message.toString(), newmsg.toString());
+    }
+
+    @Test
+    public void testAppliesTo_NullParticipantId() {
+        message = makeMessage();
+
+        assertThatThrownBy(() -> message.appliesTo(null))
+                        .isInstanceOf(NullPointerException.class);
+
+    }
+
+    @Test
+    public void testAppliesTo_ParticipantIdMatches() {
+        message = makeMessage();
+
+        // ParticipantId matches
+        ToscaConceptIdentifier id = new ToscaConceptIdentifier();
+        id.setName("id");
+        id.setVersion("1.2.3");
+        assertTrue(message.appliesTo(id));
+    }
+
+    @Test
+    public void testAppliesTo_ParticipantIdNoMatch() {
+        message = makeMessage();
+
+        // ParticipantId doesnot match
+        ToscaConceptIdentifier id = new ToscaConceptIdentifier();
+        id.setName("id1111");
+        id.setVersion("3.2.1");
+        assertFalse(message.appliesTo(id));
+        message.setParticipantId(null);
+        assertTrue(message.appliesTo(id));
+    }
+
+    private ParticipantMessage makeMessage() {
+        ParticipantMessage msg = new ParticipantMessage(ParticipantMessageType.PARTICIPANT_STATE_CHANGE);
+
+        ToscaConceptIdentifier id = new ToscaConceptIdentifier();
+        id.setName("id");
+        id.setVersion("1.2.3");
+        msg.setControlLoopId(id);
+        msg.setParticipantId(id);
+        msg.setRequestId(UUID.randomUUID());
+        msg.setTimestampMs(Long.valueOf(3000));
+
+        return msg;
+    }
+}
