f5ee1cc7e225a77635391103bd8b8ad24fbc57fa
sebdet <sebastien.determe@intl.att.com>
2021-02-12 17:18:47 +0100

    Modify Backend for listing the policies
    Improve Ui components to list policies + changes an existing camel route to expose the policy json schema properly. New files added to emulator for testing + comments added.
    Now the Ui can show the policy config
    
    Issue-ID: POLICY-3061
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>
    Change-Id: Iacf9577a4ab2cc927d05ff267e7356d8410eb77a
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- src/main/java/org/onap/policy/clamp/clds/tosca/update/parser/ToscaConverterToJsonSchema.java
+++ src/main/java/org/onap/policy/clamp/clds/tosca/update/parser/ToscaConverterToJsonSchema.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * ONAP CLAMP
  * ================================================================================
- * Copyright (C) 2020 AT&T Intellectual Property. All rights
+ * Copyright (C) 2020-2021 AT&T Intellectual Property. All rights
  *                             reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -35,6 +35,15 @@ import org.onap.policy.clamp.clds.tosca.update.parser.metadata.ToscaMetadataPars
 import org.onap.policy.clamp.clds.tosca.update.templates.JsonTemplate;
 import org.onap.policy.clamp.loop.service.Service;
 
+/**
+ * This class can be used to convert a tosca to a json schema.
+ * This class is not supposed to be used directly because it requires the json Schema templates
+ * (template conversion tosca type to json schema entry) but also the supported Tosca main type file.
+ * The class ToscaConverterWithDictionarySupport is more complete for the end user to be used (in the clamp context).
+ *
+ * @see org.onap.policy.clamp.clds.tosca.update.ToscaConverterWithDictionarySupport#convertToscaToJsonSchemaObject
+ * @see org.onap.policy.clamp.clds.tosca.update.parser.ToscaConverterToJsonSchema#getJsonSchemaOfToscaElement
+ */
 public class ToscaConverterToJsonSchema {
     private LinkedHashMap<String, ToscaElement> components;
     private LinkedHashMap<String, JsonTemplate> templates;
@@ -266,11 +275,12 @@ public class ToscaConverterToJsonSchema {
                         // Native cases
                         JsonObject itemContainer = new JsonObject();
                         String valueInEntrySchema =
-                                this.extractSpecificFieldFromMap(toscaElementProperty, "entry_schema");
+                            this.extractSpecificFieldFromMap(toscaElementProperty, "entry_schema");
                         itemContainer.addProperty("type", valueInEntrySchema);
                         propertiesInJson.add("items", itemContainer);
                         propertiesInJson.addProperty("format", "tabs-top");
                     }
+
                     // MAP Case, for now nothing
 
                     break;
