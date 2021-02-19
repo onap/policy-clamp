a5680599587c9e571f11651660700c16beb7e4ce
sebdet <sebastien.determe@intl.att.com>
2021-02-19 14:07:05 +0100

    Update clamp to 6.0.1
    - Update to 6.0.1 so that images can be released next week
    - Fix classNotFound due to update of policy models libs from 2.3.5 to 2.4.0 (ToscaPolicyIdentifier renamed to ToscaConceptIddentifier and ToscaPolicyTypeIdentifier deleted)
    
    Issue-ID: POLICY-3084
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>
    Change-Id: Ic2d3e6eb25f125ba472c77f0683081214680fafb
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>
    (cherry picked from commit eeaef28f78a23881e2f5cf0380fa1ad34472435d)

--- src/test/java/org/onap/policy/clamp/policy/pdpgroup/PoliciesPdpMergerTest.java
+++ src/test/java/org/onap/policy/clamp/policy/pdpgroup/PoliciesPdpMergerTest.java
@@ -36,8 +36,7 @@ import org.onap.policy.models.pdp.concepts.PdpGroup;
 import org.onap.policy.models.pdp.concepts.PdpGroups;
 import org.onap.policy.models.pdp.concepts.PdpSubGroup;
 import org.onap.policy.models.pdp.enums.PdpState;
-import org.onap.policy.models.tosca.authorative.concepts.ToscaPolicyIdentifier;
-import org.onap.policy.models.tosca.authorative.concepts.ToscaPolicyTypeIdentifier;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaConceptIdentifier;
 import org.skyscreamer.jsonassert.JSONAssert;
 
 /**
@@ -56,23 +55,23 @@ public class PoliciesPdpMergerTest {
         PdpSubGroup pdpSubgroup1 = new PdpSubGroup();
         pdpSubgroup1.setPdpType("subGroup1");
         pdpSubgroup1.setSupportedPolicyTypes(
-                Arrays.asList(new ToscaPolicyTypeIdentifier("org.onap.*", "1.0.0")));
+                Arrays.asList(new ToscaConceptIdentifier("org.onap.*", "1.0.0")));
 
         PdpSubGroup pdpSubgroup2 = new PdpSubGroup();
         pdpSubgroup2.setPdpType("subGroup2");
         pdpSubgroup2.setSupportedPolicyTypes(
-                Arrays.asList(new ToscaPolicyTypeIdentifier("onap.policies.monitoring.tcagen2", "1.0.0"),
-                        new ToscaPolicyTypeIdentifier("onap.policies.controlloop.operational.common.Drools", "1.0.0")));
+                Arrays.asList(new ToscaConceptIdentifier("onap.policies.monitoring.tcagen2", "1.0.0"),
+                        new ToscaConceptIdentifier("onap.policies.controlloop.operational.common.Drools", "1.0.0")));
         pdpSubgroup2.setPolicies(Arrays.asList(
-                new ToscaPolicyIdentifier("MICROSERVICE_vLoadBalancerMS_v1_0_tcagen2_1_0_0_AV0", "1.0.0")));
+                new ToscaConceptIdentifier("MICROSERVICE_vLoadBalancerMS_v1_0_tcagen2_1_0_0_AV0", "1.0.0")));
 
         PdpSubGroup pdpSubgroup3 = new PdpSubGroup();
         pdpSubgroup3.setPdpType("subGroup3");
         pdpSubgroup3.setSupportedPolicyTypes(
-                Arrays.asList(new ToscaPolicyTypeIdentifier("onap.policies.monitoring.tcagen2", "1.0.0"),
-                        new ToscaPolicyTypeIdentifier("onap.policies.controlloop.operational.common.Drools", "1.0.0")));
-        pdpSubgroup3.setPolicies(Arrays.asList(new ToscaPolicyIdentifier("org.onap.testos", "2.0.0"),
-                new ToscaPolicyIdentifier("OPERATIONAL_vLoadBalancerMS_v1_0_Drools_1_0_0_7xd", "1.0.0")));
+                Arrays.asList(new ToscaConceptIdentifier("onap.policies.monitoring.tcagen2", "1.0.0"),
+                        new ToscaConceptIdentifier("onap.policies.controlloop.operational.common.Drools", "1.0.0")));
+        pdpSubgroup3.setPolicies(Arrays.asList(new ToscaConceptIdentifier("org.onap.testos", "2.0.0"),
+                new ToscaConceptIdentifier("OPERATIONAL_vLoadBalancerMS_v1_0_Drools_1_0_0_7xd", "1.0.0")));
 
         // Should match pdpSubgroup1
         PdpGroup pdpGroup1 = new PdpGroup();
