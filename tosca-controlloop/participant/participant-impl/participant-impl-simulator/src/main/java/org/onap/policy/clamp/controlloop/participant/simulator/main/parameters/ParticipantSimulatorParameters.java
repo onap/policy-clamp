75a6574fffc4a0c926993cc5845456515ec4b3f8
Sirisha_Manchikanti <sirisha.manchikanti@est.tech>
2021-03-05 15:13:24 +0000

    Participant simulator parameters and junits
    This commit brings in main, activator, command line paramater handling
    for main, rest and database parameter handling for Participant Simulator.
    A draft SimulationHandler is included which handles simulation of participants
    and control loop elements, respective provider and participant
    intermediary handling will be shared in later commits.
    
    Issue-ID: POLICY-2987
    Change-Id: Iffbfca6907bf4199347e6349a22008ac4d491a1c
    Signed-off-by: Sirisha_Manchikanti <sirisha.manchikanti@est.tech>

---
+++ tosca-controlloop/participant/participant-impl/participant-impl-simulator/src/main/java/org/onap/policy/clamp/controlloop/participant/simulator/main/parameters/ParticipantSimulatorParameters.java
@@ -0,0 +1,49 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2021 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.clamp.controlloop.participant.simulator.main.parameters;
+
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import org.onap.policy.common.endpoints.parameters.RestServerParameters;
+import org.onap.policy.common.parameters.ParameterGroupImpl;
+import org.onap.policy.common.parameters.annotations.NotNull;
+import org.onap.policy.models.provider.PolicyModelsProviderParameters;
+
+/**
+ * Class to hold all parameters needed for the participant simulator.
+ *
+ */
+@NotNull
+@NotBlank
+@Getter
+public class ParticipantSimulatorParameters extends ParameterGroupImpl {
+    private RestServerParameters restServerParameters;
+    private PolicyModelsProviderParameters databaseProviderParameters;
+
+    /**
+     * Create the participant simulator parameter group.
+     *
+     * @param name the parameter group name
+     */
+    public ParticipantSimulatorParameters(final String name) {
+        super(name);
+    }
+}
