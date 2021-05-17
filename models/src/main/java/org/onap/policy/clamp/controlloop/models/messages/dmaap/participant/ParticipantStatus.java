b99385e66f706075340b47cc898a949dc15d8d46
Sirisha_Manchikanti <sirisha.manchikanti@est.tech>
2021-05-17 11:02:27 +0100

    ControlLoop Models
    This commit introduces models directory which hosts model files used
    by controlloop modules like runtime, participants etc
    
    Issue-ID: POLICY-3215
    Signed-off-by: Sirisha_Manchikanti <sirisha.manchikanti@est.tech>
    Change-Id: Ie16f5e6a1004d88e996c321ed24d587818b8c770

---
+++ models/src/main/java/org/onap/policy/clamp/controlloop/models/messages/dmaap/participant/ParticipantStatus.java
@@ -0,0 +1,76 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2021 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.clamp.controlloop.models.messages.dmaap.participant;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.onap.policy.clamp.controlloop.models.controlloop.concepts.ControlLoops;
+import org.onap.policy.clamp.controlloop.models.controlloop.concepts.ParticipantHealthStatus;
+import org.onap.policy.clamp.controlloop.models.controlloop.concepts.ParticipantState;
+import org.onap.policy.clamp.controlloop.models.controlloop.concepts.ParticipantStatistics;
+
+/**
+ * Class to represent the PARTICIPANT_STATUS message that all the participants send to the control loop runtime.
+ */
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class ParticipantStatus extends ParticipantMessage {
+    // The response should be completed if this message is a response to a request from the Control Loop Runtime
+    private ParticipantResponseDetails response;
+
+    // State and health status of the participant
+    private ParticipantState state;
+    private ParticipantHealthStatus healthStatus;
+
+    // Control Loops on the participant
+    private ControlLoops controlLoops;
+
+    // Participant statistics
+    private ParticipantStatistics participantStatistics;
+
+    // Description. May be left {@code null}.
+    private String message;
+
+    /**
+     * Constructor for instantiating ParticipantStatus class with message name.
+     *
+     */
+    public ParticipantStatus() {
+        super(ParticipantMessageType.PARTICIPANT_STATUS);
+    }
+
+    /**
+     * Constructs the object, making a deep copy.
+     *
+     * @param source source from which to copy
+     */
+    public ParticipantStatus(final ParticipantStatus source) {
+        super(source);
+
+        this.state = source.state;
+        this.healthStatus = source.healthStatus;
+        this.message = source.message;
+        this.controlLoops = (source.controlLoops == null ? null : new ControlLoops(source.controlLoops));
+        this.response = (source.response == null ? null : new ParticipantResponseDetails(source.response));
+    }
+}
