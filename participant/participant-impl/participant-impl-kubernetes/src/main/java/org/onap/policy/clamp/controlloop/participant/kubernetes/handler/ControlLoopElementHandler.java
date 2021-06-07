f0322a9fa428fe27dfbceeeebe56f406e62e6ce5
liamfallon <liam.fallon@est.tech>
2021-06-07 16:41:12 +0100

    Initial commit for kubernetes participant in CLAMP
    Spring application that exposes REST end points for installing, uninstalling, onboarding
    and deleting of helm charts to/from local directory.
    CL runtime can also trigger installation and uninstallation of helm charts from both local and configured helm repositories.
    
    Junits will be committed as a separate review.
    
    Issue-ID: POLICY-3240
    Change-Id: I7633b6fd6ad41fc8fa55d3722e44f1b2ec132e50
    Signed-off-by: zrrmmua <ramesh.murugan.iyer@est.tech>
    Signed-off-by: liamfallon <liam.fallon@est.tech>

---
+++ participant/participant-impl/participant-impl-kubernetes/src/main/java/org/onap/policy/clamp/controlloop/participant/kubernetes/handler/ControlLoopElementHandler.java
@@ -0,0 +1,148 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2021 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.clamp.controlloop.participant.kubernetes.handler;
+
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import org.onap.policy.clamp.controlloop.models.controlloop.concepts.ControlLoopElement;
+import org.onap.policy.clamp.controlloop.models.controlloop.concepts.ControlLoopOrderedState;
+import org.onap.policy.clamp.controlloop.models.controlloop.concepts.ControlLoopState;
+import org.onap.policy.clamp.controlloop.participant.intermediary.api.ControlLoopElementListener;
+import org.onap.policy.clamp.controlloop.participant.intermediary.api.ParticipantIntermediaryApi;
+import org.onap.policy.clamp.controlloop.participant.kubernetes.exception.ServiceException;
+import org.onap.policy.clamp.controlloop.participant.kubernetes.models.ChartInfo;
+import org.onap.policy.clamp.controlloop.participant.kubernetes.service.ChartService;
+import org.onap.policy.models.base.PfModelException;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaNodeTemplate;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaServiceTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * This class handles implementation of controlLoopElement updates.
+ */
+@Component
+public class ControlLoopElementHandler implements ControlLoopElementListener {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+
+    @Autowired
+    private ChartService chartService;
+
+    @Autowired
+    private ParticipantIntermediaryApi intermediaryApi;
+
+    // Map of CLElement Id and installed Helm charts
+    private static final Map<String, ChartInfo> chartMap = new HashMap<>();
+
+    /**
+     * Callback method to handle a control loop element state change.
+     *
+     * @param controlLoopElementId the ID of the control loop element
+     * @param currentState the current state of the control loop element
+     * @param newState the state to which the control loop element is changing to
+     */
+    @Override
+    public void controlLoopElementStateChange(UUID controlLoopElementId, ControlLoopState currentState,
+            ControlLoopOrderedState newState) {
+        switch (newState) {
+            case UNINITIALISED:
+                ChartInfo chart = chartMap.get(controlLoopElementId.toString());
+                if (chart != null) {
+                    LOGGER.info("Helm deployment to be deleted {} ", chart.getReleaseName());
+                    try {
+                        chartService.uninstallChart(chart);
+                        intermediaryApi.updateControlLoopElementState(controlLoopElementId, newState,
+                                ControlLoopState.UNINITIALISED);
+                    } catch (ServiceException se) {
+                        LOGGER.warn("deletion of Helm deployment failed", se);
+                    }
+                }
+                break;
+            case PASSIVE:
+                intermediaryApi.updateControlLoopElementState(controlLoopElementId, newState, ControlLoopState.PASSIVE);
+                break;
+            case RUNNING:
+                intermediaryApi.updateControlLoopElementState(controlLoopElementId, newState, ControlLoopState.RUNNING);
+                break;
+            default:
+                LOGGER.warn("cannot transition from state {} to state {}", currentState, newState);
+                break;
+        }
+    }
+
+
+    /**
+     * Callback method to handle an update on a control loop element.
+     *
+     * @param element the information on the control loop element
+     * @param controlLoopDefinition toscaServiceTemplate
+     * @throws PfModelException in case of an exception
+     */
+    @Override
+    public void controlLoopElementUpdate(ControlLoopElement element, ToscaServiceTemplate controlLoopDefinition)
+            throws PfModelException {
+
+        for (Map.Entry<String, ToscaNodeTemplate> nodeTemplate : controlLoopDefinition.getToscaTopologyTemplate()
+                .getNodeTemplates().entrySet()) {
+
+            // Fetching the node template of corresponding CL element
+            if (element.getDefinition().getName().equals(nodeTemplate.getKey())
+                    && nodeTemplate.getValue().getProperties().containsKey("chart")) {
+                @SuppressWarnings("unchecked")
+                Map<String, Object> chartData =
+                        (Map<String, Object>) nodeTemplate.getValue().getProperties().get("chart");
+
+                LOGGER.info("Installation request received for the Helm Chart {} ", chartData);
+                var chart = new ChartInfo(String.valueOf(chartData.get("release_name")),
+                        String.valueOf(chartData.get("chart_name")), String.valueOf(chartData.get("version")),
+                        String.valueOf(chartData.get("namespace")));
+                try {
+                    var repositoryValue = chartData.get("repository");
+                    if (repositoryValue != null) {
+                        chart.setRepository(String.valueOf(repositoryValue));
+                    }
+                    chartService.installChart(chart);
+                    chartMap.put(element.getId().toString(), chart);
+                } catch (IOException | ServiceException ise) {
+                    LOGGER.warn("installation of Helm chart failed", ise);
+                }
+            }
+        }
+    }
+
+    /**
+     * Overridden method.
+     *
+     * @param controlLoopElementId controlLoopElement id
+     * @throws PfModelException incase of error
+     */
+    @Override
+    public void handleStatistics(UUID controlLoopElementId) throws PfModelException {
+        // TODO Implement statistics functionality
+    }
+}
---
+++ participant/participant-impl/participant-impl-kubernetes/src/main/java/org/onap/policy/clamp/controlloop/participant/kubernetes/helm/HelmClient.java
@@ -0,0 +1,201 @@
+/*-
+ * ========================LICENSE_START=================================
+ * Copyright (C) 2021 Nordix Foundation. All rights reserved.
+ * ======================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================LICENSE_END===================================
+ */
+
+package org.onap.policy.clamp.controlloop.participant.kubernetes.helm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.invoke.MethodHandles;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.commons.io.IOUtils;
+import org.onap.policy.clamp.controlloop.participant.kubernetes.exception.ServiceException;
+import org.onap.policy.clamp.controlloop.participant.kubernetes.models.ChartInfo;
+import org.onap.policy.clamp.controlloop.participant.kubernetes.service.ChartStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Client to talk with Helm cli. Supports helm3 + version
+ */
+@Component
+public class HelmClient {
+
+    @Autowired
+    private ChartStore chartStore;
+
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+
+    /**
+     * Install a chart.
+     *
+     * @param chart name and version.
+     * @throws ServiceException incase of error
+     */
+    public void installChart(ChartInfo chart) throws ServiceException {
+        var processBuilder = prepareCreateNamespaceCommand(chart.getNamespace());
+        try {
+            executeCommand(processBuilder);
+        } catch (ServiceException e) {
+            logger.warn("Namespace not created", e);
+        }
+        processBuilder = prepareInstallCommand(chart);
+        logger.info("Installing helm chart {} from the repository {} ", chart.getChartName(), chart.getRepository());
+        executeCommand(processBuilder);
+        logger.info("Chart {} installed successfully", chart.getChartName());
+    }
+
+    /**
+     * Finds helm chart repository for the chart.
+     *
+     * @param chart ChartInfo.
+     * @throws ServiceException incase of error
+     */
+    public String findChartRepository(ChartInfo chart) throws ServiceException, IOException {
+        updateHelmRepo();
+        logger.info("Looking for helm chart {} in all the configured helm repositories", chart.getChartName());
+        String repository = null;
+
+        var process = helmRepoVerifyCommand(chart.getChartName()).start();
+
+        try (var reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line = reader.readLine();
+            while (line != null) {
+                if (line.contains(chart.getChartName())) {
+                    repository = line.split("/")[0];
+                    logger.info("Helm chart located in the repository {} ", repository);
+                    return repository;
+                }
+                line = reader.readLine();
+            }
+        }
+
+        var localHelmChartDir = chartStore.getAppPath(chart.getChartName(), chart.getVersion()).toString();
+        logger.info("Chart not found in helm repositories, verifying local repo {} ", localHelmChartDir);
+        if (verifyLocalHelmRepo(localHelmChartDir + "/" + chart.getChartName())) {
+            repository = localHelmChartDir;
+        }
+
+        return repository;
+    }
+
+    /**
+     * Uninstall a chart.
+     *
+     * @param chart name and version.
+     * @throws ServiceException incase of error
+     */
+    public void uninstallChart(ChartInfo chart) throws ServiceException {
+        executeCommand(prepareUnInstallCommand(chart));
+    }
+
+    static String executeCommand(ProcessBuilder processBuilder) throws ServiceException {
+        var commandStr = toString(processBuilder);
+
+        processBuilder.redirectInput(ProcessBuilder.Redirect.DISCARD);
+
+        try {
+            var process = processBuilder.start();
+            process.waitFor();
+            int exitValue = process.exitValue();
+
+            if (exitValue != 0) {
+                var error = IOUtils.toString(process.getErrorStream(), StandardCharsets.UTF_8);
+                throw new ServiceException("Command execution failed: " + commandStr + " " + error);
+            }
+            var output = IOUtils.toString(process.getInputStream(), StandardCharsets.UTF_8);
+            logger.debug("Command <{}> execution, output: {}", commandStr, output);
+            return output;
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+            throw new ServiceException(
+                    "Failed to execute the Command: " + commandStr + ", the command was interrupted", ie);
+        } catch (Exception exc) {
+            throw new ServiceException("Failed to execute the Command: " + commandStr, exc);
+        }
+    }
+
+    private ProcessBuilder prepareInstallCommand(ChartInfo chart) {
+
+        // @formatter:off
+        List<String> helmArguments = new ArrayList<>(
+                Arrays.asList(
+                        "helm",
+                        "install", chart.getReleaseName(), chart.getRepository() + "/" + chart.getChartName(),
+                        "--version", chart.getVersion(),
+                        "--namespace", chart.getNamespace()
+                )
+        );
+        // @formatter:on
+
+        // Verify if values.yaml available for the chart
+        var overrideFile = chartStore.getOverrideFile(chart).getPath();
+        if (verifyLocalHelmRepo(overrideFile)) {
+            logger.info("Override yaml file available for the helm chart");
+            helmArguments.addAll(Arrays.asList("--values", overrideFile));
+        }
+
+        return new ProcessBuilder().command(helmArguments);
+    }
+
+    private ProcessBuilder prepareUnInstallCommand(ChartInfo chart) {
+        return new ProcessBuilder("helm", "delete", chart.getReleaseName(), "--namespace", chart.getNamespace());
+    }
+
+    private ProcessBuilder prepareCreateNamespaceCommand(String namespace) {
+        return new ProcessBuilder(Arrays.asList("kubectl", "create", "namespace", namespace));
+    }
+
+    private ProcessBuilder helmRepoVerifyCommand(String chartName) {
+        return new ProcessBuilder().command(Arrays.asList("bash", "-c", "helm search repo | grep " + chartName));
+    }
+
+    private ProcessBuilder localRepoVerifyCommand(String localFile) {
+        return new ProcessBuilder(Arrays.asList("bash", "-c", "ls " + localFile));
+    }
+
+    private void updateHelmRepo() throws ServiceException {
+        logger.info("Updating local helm repositories before verifying the chart");
+        List<String> helmArguments = Arrays.asList("helm", "repo", "update");
+
+        executeCommand(new ProcessBuilder().command(helmArguments));
+        logger.debug("Helm repositories updated successfully");
+    }
+
+    private boolean verifyLocalHelmRepo(String localFile) {
+        var isVerified = false;
+        var processBuilder = localRepoVerifyCommand(localFile);
+        try {
+            executeCommand(processBuilder);
+            isVerified = true;
+        } catch (ServiceException e) {
+            logger.error("Unable to verify file in local repository", e);
+        }
+        return isVerified;
+    }
+
+    protected static String toString(ProcessBuilder processBuilder) {
+        return String.join(" ", processBuilder.command());
+    }
+}
