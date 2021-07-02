4f1094f8e9070b36af8f9d172c3398e256609234
Sirisha_Manchikanti <sirisha.manchikanti@est.tech>
2021-07-02 08:53:07 +0100

    Add models participant register,deregister and update messages
    This commit adds models for ParticipantRegister, ParticipantDeregister and
    ParticipantUpdate messages. Respective handling will be taken care in a
    later commit.
    
    Issue-ID: POLICY-3414
    Signed-off-by: Sirisha_Manchikanti <sirisha.manchikanti@est.tech>
    Change-Id: I622b98b03df09eecaac160ba564c8a21b449c642

---
+++ models/src/main/java/org/onap/policy/clamp/controlloop/models/controlloop/concepts/ControlLoopElementDefinition.java
@@ -0,0 +1,62 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2021 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.clamp.controlloop.models.controlloop.concepts;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.UUID;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
+import org.onap.policy.models.base.PfUtils;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaServiceTemplate;
+
+/**
+ * Class to represent a control loop element definition instance.
+ */
+@NoArgsConstructor
+@Data
+@ToString
+public class ControlLoopElementDefinition {
+
+    @NonNull
+    private UUID id = UUID.randomUUID();
+
+    // The definition of the Control Loop Element in TOSCA
+    private ToscaServiceTemplate controlLoopElementToscaServiceTemplate;
+
+    // A map indexed by the property name. Each map entry is the serialized value of the property,
+    // which can be deserialized into an instance of the type of the property.
+    private Map<String, String> commonPropertiesMap = new LinkedHashMap<>();
+
+    /**
+     * Copy constructor, does a deep copy but as all fields here are immutable, it's just a regular copy.
+     *
+     * @param clElementDefinition the controlloop element definition to copy from
+     */
+    public ControlLoopElementDefinition(final ControlLoopElementDefinition clElementDefinition) {
+        this.id = clElementDefinition.id;
+        this.controlLoopElementToscaServiceTemplate =
+                new ToscaServiceTemplate(clElementDefinition.controlLoopElementToscaServiceTemplate);
+        this.commonPropertiesMap = PfUtils.mapMap(clElementDefinition.commonPropertiesMap, String::new);
+    }
+}
---
+++ models/src/test/java/org/onap/policy/clamp/controlloop/models/messages/dmaap/participant/ParticipantUpdateTest.java
@@ -0,0 +1,90 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2021 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.clamp.controlloop.models.messages.dmaap.participant;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.onap.policy.clamp.controlloop.models.messages.dmaap.participant.ParticipantMessageUtils.removeVariableFields;
+
+import java.time.Instant;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.UUID;
+import org.junit.Test;
+import org.onap.policy.clamp.controlloop.models.controlloop.concepts.ControlLoopElementDefinition;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaConceptIdentifier;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaServiceTemplate;
+
+/**
+ * Test the copy constructor.
+ */
+public class ParticipantUpdateTest {
+    @Test
+    public void testCopyConstructor() {
+        assertThatThrownBy(() -> new ParticipantUpdate(null)).isInstanceOf(NullPointerException.class);
+
+        ParticipantUpdate orig = new ParticipantUpdate();
+        // verify with all values
+        ToscaConceptIdentifier id = new ToscaConceptIdentifier();
+        id.setName("id");
+        id.setVersion("1.2.3");
+        orig.setControlLoopId(id);
+        orig.setParticipantId(id);
+        orig.setParticipantType(id);
+        orig.setMessageId(UUID.randomUUID());
+        orig.setTimestamp(Instant.ofEpochMilli(3000));
+
+        ToscaServiceTemplate toscaServiceTemplate = new ToscaServiceTemplate();
+        toscaServiceTemplate.setName("serviceTemplate");
+        toscaServiceTemplate.setDerivedFrom("parentServiceTemplate");
+        toscaServiceTemplate.setDescription("Description of serviceTemplate");
+        toscaServiceTemplate.setVersion("1.2.3");
+        orig.setControlLoopElementToscaServiceTemplate(toscaServiceTemplate);
+
+        ControlLoopElementDefinition clDefinition = new ControlLoopElementDefinition();
+        clDefinition.setId(UUID.randomUUID());
+        clDefinition.setControlLoopElementToscaServiceTemplate(toscaServiceTemplate);
+        Map<String, String> commonPropertiesMap = new LinkedHashMap<>();
+        commonPropertiesMap.put("Prop1", "PropValue");
+        clDefinition.setCommonPropertiesMap(commonPropertiesMap);
+        orig.setControlLoopElementDefinition(clDefinition);
+
+        orig.setCommonPropertiesMap(commonPropertiesMap);
+
+        Map<UUID, ControlLoopElementDefinition> controlLoopElementDefinitionMap = new LinkedHashMap<>();
+        controlLoopElementDefinitionMap.put(UUID.randomUUID(), clDefinition);
+        orig.setControlLoopElementDefinitionMap(controlLoopElementDefinitionMap);
+
+        Map<ToscaConceptIdentifier, Map<UUID, ControlLoopElementDefinition>>
+            participantDefinitionUpdateMap = new LinkedHashMap<>();
+        participantDefinitionUpdateMap.put(id, controlLoopElementDefinitionMap);
+        orig.setParticipantDefinitionUpdateMap(participantDefinitionUpdateMap);
+
+        ParticipantUpdate other = new ParticipantUpdate(orig);
+
+        assertEquals(removeVariableFields(orig.toString()), removeVariableFields(other.toString()));
+
+        // ensure list and items are not the same object
+        assertNotSame(other.getControlLoopElementDefinition(), clDefinition);
+        assertNotSame(other.getControlLoopElementToscaServiceTemplate(), toscaServiceTemplate);
+    }
+}
