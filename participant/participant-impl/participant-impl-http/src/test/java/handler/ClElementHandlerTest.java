a899f6d9ff90e0112a3c8ff413b379c3a0769e9d
rameshiyer27 <ramesh.murugan.iyer@est.tech>
2021-07-20 07:57:41 +0100

    Implement http participant in CLAMP
    Issue-ID: POLICY-3449
    Signed-off-by: zrrmmua <ramesh.murugan.iyer@est.tech>
    Change-Id: Ibfe23a6e98fb760a930b6080dc2291113b3cb4fa

---
+++ participant/participant-impl/participant-impl-http/src/test/java/handler/ClElementHandlerTest.java
@@ -0,0 +1,63 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2021 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package handler;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.onap.policy.clamp.controlloop.models.controlloop.concepts.ControlLoopElement;
+import org.onap.policy.clamp.controlloop.participant.http.main.handler.ControlLoopElementHandler;
+import org.onap.policy.common.utils.coder.CoderException;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaServiceTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import utils.CommonTestData;
+import utils.ToscaUtils;
+
+@ExtendWith(SpringExtension.class)
+class ClElementHandlerTest {
+
+    @InjectMocks
+    @Spy
+    private ControlLoopElementHandler controlLoopElementHandler = new ControlLoopElementHandler();
+
+    private CommonTestData commonTestData = new CommonTestData();
+
+    private static ToscaServiceTemplate serviceTemplate;
+
+    @BeforeAll
+    static void init() throws CoderException {
+        serviceTemplate = ToscaUtils.readControlLoopFromTosca();
+    }
+
+    @Test
+    void test_ControlLoopElementUpdate() {
+        doNothing().when(controlLoopElementHandler).invokeHttpClient(any());
+        ControlLoopElement element = commonTestData.getControlLoopElement();
+        assertDoesNotThrow(() -> controlLoopElementHandler
+            .controlLoopElementUpdate(element, serviceTemplate));
+    }
+}
