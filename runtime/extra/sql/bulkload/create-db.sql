d0a4d4c86c1134b74b7b7e299b8297c62861bf17
saul.gill <saul.gill@est.tech>
2021-07-23 10:48:25 +0100

    Added endpoint for common or instance properties
    Runtime-controlloop and Camel endpoionts added
    Flag common can be used - true common props - false - instance props
    
    Changed getServiceTemplate endpoint to return less
    
    Add fix for issue where constraints array with one null
    element couldn't be accepted when commissioning a
    service template in json format
    
    Added creation of controlloop db to clamp scripts
    
    Issue-ID: POLICY-3439
    Change-Id: I9d189ca030868b47b46a2e0bc5e731c23fba2a61
    Signed-off-by: saul.gill <saul.gill@est.tech>

--- runtime/extra/sql/bulkload/create-db.sql
+++ runtime/extra/sql/bulkload/create-db.sql
@@ -7,5 +7,9 @@ USE `cldsdb4`;
 DROP USER 'clds';
 CREATE USER 'clds';
 GRANT ALL on cldsdb4.* to 'clds' identified by 'sidnnd83K' with GRANT OPTION;
+CREATE DATABASE `controlloop`;
+USE `controlloop`;
+DROP USER 'policy';
+CREATE USER 'policy';
+GRANT ALL on controlloop.* to 'policy' identified by 'P01icY' with GRANT OPTION;
 FLUSH PRIVILEGES;
-
