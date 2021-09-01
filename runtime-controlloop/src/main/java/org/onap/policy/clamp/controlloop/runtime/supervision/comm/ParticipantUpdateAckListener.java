5af35328dd1cab498c1d361126fd123dd859eb07
Jim Hahn <jrh3@att.com>
2021-09-01 17:50:02 -0400

    Fix sonars in clamp #2
    Fixed sonars:
    
    Fixed eclipse warnings:
    - unused imports
    - unused fields
    - add serialVersionUID
    
    Issue-ID: POLICY-3200
    Change-Id: Ieaab6e85f65fb5eed6db337060961ba831628905
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- runtime-controlloop/src/main/java/org/onap/policy/clamp/controlloop/runtime/supervision/comm/ParticipantUpdateAckListener.java
+++ runtime-controlloop/src/main/java/org/onap/policy/clamp/controlloop/runtime/supervision/comm/ParticipantUpdateAckListener.java
@@ -1,6 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
  * Copyright (C) 2021 Nordix Foundation.
+ * Modifications Copyright (C) 2021 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +36,8 @@ import org.springframework.stereotype.Component;
  * Listener for ParticipantUpdateAck messages sent by participants.
  */
 @Component
-public class ParticipantUpdateAckListener extends ScoListener<ParticipantUpdateAck> implements Listener {
+public class ParticipantUpdateAckListener extends ScoListener<ParticipantUpdateAck>
+                implements Listener<ParticipantUpdateAck> {
     private static final Logger LOGGER = LoggerFactory.getLogger(ParticipantUpdateAckListener.class);
 
     private final SupervisionHandler supervisionHandler;
