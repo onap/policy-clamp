fa9cbe9ed943621612f187b37e382889b7a93432
Jim Hahn <jrh3@att.com>
2021-09-01 17:50:02 -0400

    Fix sonars in clamp #2
    Fixed sonars:
    
    Fixed eclipse warnings:
    - unused imports
    - unused fields
    - add serialVersionUID
    
    Issue-ID: POLICY-3200
    Change-Id: Ieaab6e85f65fb5eed6db337060961ba831628905
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- models/src/main/java/org/onap/policy/clamp/controlloop/models/messages/dmaap/participant/ParticipantUpdate.java
+++ models/src/main/java/org/onap/policy/clamp/controlloop/models/messages/dmaap/participant/ParticipantUpdate.java
@@ -1,6 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
  * Copyright (C) 2021 Nordix Foundation.
+ * Modifications Copyright (C) 2021 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +28,6 @@ import lombok.Setter;
 import lombok.ToString;
 import org.onap.policy.clamp.controlloop.models.controlloop.concepts.ParticipantDefinition;
 import org.onap.policy.models.base.PfUtils;
-import org.onap.policy.models.tosca.authorative.concepts.ToscaServiceTemplate;
 
 /**
  * Class to represent the PARTICIPANT_UPDATE message that the control loop runtime sends to a participant.
