d5646454fbf5d240278ee75dd6abcc4c10d0f48b
rameshiyer27 <ramesh.murugan.iyer@est.tech>
2022-07-28 11:58:19 +0100

    Add module for AC Element implementation
    Refactored unused participant simulator module to implement AC element
    microservice for ACM testing purposes. Empty spring project added.
    Business logics will be added as a separate review on top of this structure.
    
    Issue-ID: POLICY-4312
    Signed-off-by: zrrmmua <ramesh.murugan.iyer@est.tech>
    Change-Id: I8fc280083ce4b759ed9ab23b223cf1555176ef62

---
+++ participant/participant-impl/participant-impl-acelement/src/main/java/org/onap/policy/clamp/acm/element/main/rest/GlobalControllerExceptionHandler.java
@@ -0,0 +1,45 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2021-2022 Nordix Foundation.
+ * ================================================================================
+ * Modifications Copyright (C) 2021 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.clamp.acm.element.main.rest;
+
+import org.onap.policy.clamp.common.acm.exception.AutomationCompositionException;
+import org.onap.policy.clamp.models.acm.messages.rest.SimpleResponse;
+import org.onap.policy.clamp.models.acm.rest.RestUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    /**
+     * Handle AutomationCompositionException.
+     *
+     * @param ex AutomationCompositionException
+     * @return ResponseEntity
+     */
+    @ExceptionHandler(AutomationCompositionException.class)
+    public ResponseEntity<SimpleResponse> handleBadRequest(AutomationCompositionException ex) {
+        return RestUtils.toSimpleResponse(ex);
+    }
+}
