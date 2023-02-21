0f1a5d6c6f603fb6d8027d9f1e297190eb1485d6
liamfallon <liam.fallon@est.tech>
2023-02-21 09:09:15 +0000

    Fix build in kserver participant
    The kserve participant build breaks due to refactoring in other classes.
    
    Issue-ID: POLICY-4482
    Change-Id: I023fb74faad4b3b0d80d9fb8771d9b58497db191
    Signed-off-by: liamfallon <liam.fallon@est.tech>

--- participant/participant-impl/participant-impl-kserve/src/main/java/org/onap/policy/clamp/acm/participant/kserve/k8s/InferenceServiceValidator.java
+++ participant/participant-impl/participant-impl-kserve/src/main/java/org/onap/policy/clamp/acm/participant/kserve/k8s/InferenceServiceValidator.java
@@ -25,7 +25,6 @@ import lombok.SneakyThrows;
 import org.onap.policy.clamp.acm.participant.kserve.exception.KserveException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class InferenceServiceValidator implements Runnable {
 
