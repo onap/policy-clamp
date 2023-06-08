5006eef32b196e3dbc95fe1a0d8cd33a8ffab5b4
FrancescoFioraEst <francesco.fiora@est.tech>
2023-06-08 12:19:02 +0100

    Add pom structure for mock participant in ACM
    Issue-ID: POLICY-4723
    Change-Id: I43049431327f4126b2e66fc456e876d57388f604
    Signed-off-by: FrancescoFioraEst <francesco.fiora@est.tech>

---
+++ participant/participant-impl/participant-impl-simulator/src/test/java/org/onap/policy/clamp/acm/participant/sim/parameters/ParticipantSimParametersTest.java
@@ -0,0 +1,45 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2023 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.clamp.acm.participant.sim.parameters;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.validation.Validation;
+import javax.validation.ValidatorFactory;
+import org.junit.jupiter.api.Test;
+import org.onap.policy.clamp.acm.participant.sim.comm.CommonTestData;
+
+class ParticipantSimParametersTest {
+
+    private final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+
+    @Test
+    void testNotValidParticipantSimParameters() {
+        var participantParameters = new ParticipantSimParameters();
+        assertThat(validatorFactory.getValidator().validate(participantParameters)).isNotEmpty();
+    }
+
+    @Test
+    void testValidParticipantSimParameters() {
+        var participantParameters = CommonTestData.getParticipantSimParameters();
+        assertThat(validatorFactory.getValidator().validate(participantParameters)).isEmpty();
+    }
+}
