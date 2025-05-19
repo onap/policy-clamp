469344fe52b815aac13354a3ac1fb82364e142d4
rameshiyer27 <ramesh.murugan.iyer@est.tech>
2025-05-19 13:25:43 +0100

    Remove large object from jpa classes
    Issue-ID: POLICY-5365
    Signed-off-by: rameshiyer27 <ramesh.murugan.iyer@est.tech>
    Change-Id: I2050cd7a127f4cf951ce0b4c518b53451b03f08f

--- models/src/main/java/org/onap/policy/clamp/models/acm/persistence/concepts/JpaMessage.java
+++ models/src/main/java/org/onap/policy/clamp/models/acm/persistence/concepts/JpaMessage.java
@@ -1,6 +1,6 @@
 /*-
  * ============LICENSE_START=======================================================
- * Copyright (C) 2025 Nordix Foundation.
+ * Copyright (C) 2025 OpenInfra Foundation Europe. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
-import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -59,7 +58,6 @@ public class JpaMessage extends Validated implements PfAuthorative<DocMessage> {
     @NotNull
     private Timestamp lastMsg = TimestampHelper.nowTimestamp();
 
-    @Lob
     @Column(length = 100000)
     @Convert(converter = StringToDocMessage.class)
     @NotNull
