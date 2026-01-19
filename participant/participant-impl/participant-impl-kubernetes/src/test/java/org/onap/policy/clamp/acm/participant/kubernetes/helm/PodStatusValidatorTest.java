70f06131a1c44c4d68111a757f6ab4990d366212
FrancescoFioraEst <francesco.fiora@est.tech>
2026-01-19 11:49:12 +0000

    Refactor participant-Kubernetes
    Refactor participant-Kubernetes and remove duplication
    of Spring config file.
    
    Issue-ID: POLICY-5533
    Change-Id: I3cfe04d4eadde82b476f6131eecb7958c311b5fa
    Signed-off-by: FrancescoFioraEst <francesco.fiora@est.tech>

--- participant/participant-impl/participant-impl-kubernetes/src/test/java/org/onap/policy/clamp/acm/participant/kubernetes/helm/PodStatusValidatorTest.java
+++ participant/participant-impl/participant-impl-kubernetes/src/test/java/org/onap/policy/clamp/acm/participant/kubernetes/helm/PodStatusValidatorTest.java
@@ -1,6 +1,6 @@
 /*-
  * ============LICENSE_START=======================================================
- *  Copyright (C) 2021-2022,2024 Nordix Foundation.
+ *  Copyright (C) 2021-2022,2024,2026 OpenInfra Foundation Europe. All rights reserved.
  *  Modifications Copyright (C) 2021 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,14 +25,11 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 import java.io.File;
-import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.onap.policy.clamp.acm.participant.kubernetes.exception.ServiceException;
 import org.onap.policy.clamp.acm.participant.kubernetes.models.ChartInfo;
 import org.onap.policy.clamp.acm.participant.kubernetes.models.ChartList;
@@ -40,54 +37,49 @@ import org.onap.policy.common.utils.coder.Coder;
 import org.onap.policy.common.utils.coder.CoderException;
 import org.onap.policy.common.utils.coder.StandardCoder;
 import org.onap.policy.models.base.PfModelException;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 class PodStatusValidatorTest {
 
     private static final Coder CODER = new StandardCoder();
     private static final String CHART_INFO_YAML = "src/test/resources/ChartList.json";
     private static final int TIMEOUT = 2;
     private static final int STATUS_CHECK_INTERVAL = 1;
-    private static List<ChartInfo> charts;
-
-    @InjectMocks
-    private PodStatusValidator podStatusValidator = new PodStatusValidator(charts.get(0), TIMEOUT,
-            STATUS_CHECK_INTERVAL);
-
-    @InjectMocks
-    private PodStatusValidator podValidatorWithPodName = new PodStatusValidator(charts.get(2), TIMEOUT,
-            STATUS_CHECK_INTERVAL);
-
-
-    @Mock
-    private HelmClient client;
+    private static ChartInfo chart0;
+    private static ChartInfo chart2;
 
     @BeforeAll
     static void init() throws CoderException {
-        charts = CODER.decode(new File(CHART_INFO_YAML), ChartList.class).getCharts();
+        var charts = CODER.decode(new File(CHART_INFO_YAML), ChartList.class).getCharts();
+        chart0 = charts.get(0);
+        chart2 = charts.get(2);
     }
-
 
     @Test
     void test_RunningPodState() throws ServiceException {
+        var client = mock(HelmClient.class);
+        var podStatusValidator = new PodStatusValidator(client);
         String runningPod = "NAME\tREADY\tSTATUS\tRESTARTS\tAGE\r\nHelloWorld-54777df9f8-qpzqr\t1/1\tRunning\t0\t9h";
         doReturn(runningPod).when(client).executeCommand(any());
-        assertDoesNotThrow(() -> podStatusValidator.run());
+        assertDoesNotThrow(() -> podStatusValidator.run(TIMEOUT, STATUS_CHECK_INTERVAL, chart0));
     }
 
     @Test
     void test_InvalidPodState() throws ServiceException {
+        var client = mock(HelmClient.class);
+        var podStatusValidator = new PodStatusValidator(client);
         String invalidPod = "NAME\tREADY\tSTATUS\tRESTARTS\tAGE\nhellofromdocker-54777df9f8-qpzqr\t1/1\tInit\t0\t9h";
         doReturn(invalidPod).when(client).executeCommand(any());
-        assertThrows(PfModelException.class, () -> podStatusValidator.run());
+        assertThrows(PfModelException.class,
+                () -> podStatusValidator.run(TIMEOUT, STATUS_CHECK_INTERVAL, chart0));
     }
 
     // Use case scenario: Hard coded pod name
     @Test
     void test_RunningPodStateWithPodName() throws ServiceException {
+        var client = mock(HelmClient.class);
+        var podStatusValidator = new PodStatusValidator(client);
         String runningPod = "NAME\tREADY\tSTATUS\tRESTARTS\tAGE\r\nhelloallworld-54777df9f8-qpzqr\t1/1\tRunning\t0\t9h";
         doReturn(runningPod).when(client).executeCommand(any());
-        assertDoesNotThrow(() -> podValidatorWithPodName.run());
+        assertDoesNotThrow(() -> podStatusValidator.run(TIMEOUT, STATUS_CHECK_INTERVAL, chart2));
     }
 }
