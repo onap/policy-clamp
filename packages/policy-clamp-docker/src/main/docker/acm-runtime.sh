30f0fa5b51a4f45530cb1cfd9714aeb22be96e10
FrancescoFioraEst <francesco.fiora@est.tech>
2026-01-26 14:10:00 +0000

    Remove duplicate Spring config files from ACM-R
    Issue-ID: POLICY-5511
    Change-Id: Id27befafcfdcfd02ff3db48f7f1d94f91a38d959
    Signed-off-by: FrancescoFioraEst <francesco.fiora@est.tech>

--- packages/policy-clamp-docker/src/main/docker/acm-runtime.sh
+++ packages/policy-clamp-docker/src/main/docker/acm-runtime.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 #
 # ============LICENSE_START=======================================================
-#  Copyright (C) 2021-2022, 2025 OpenInfra Foundation Europe. All rights reserved.
+#  Copyright (C) 2021-2022, 2025,2026 OpenInfra Foundation Europe. All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,16 +19,6 @@
 # ============LICENSE_END=========================================================
 #
 
-if [ "$#" -eq 1 ]; then
-    CONFIG_FILE=$1
-fi
-
-if [ -z "$CONFIG_FILE" ]; then
-    CONFIG_FILE="${POLICY_HOME}/etc/AcRuntimeParameters.yaml"
-fi
-
-echo "Policy clamp runtime acm config file: $CONFIG_FILE"
-
 if [ -f "${POLICY_HOME}/etc/mounted/logback.xml" ]; then
     echo "overriding logback xml file"
     cp -f "${POLICY_HOME}"/etc/mounted/logback.xml "${POLICY_HOME}"/etc/
@@ -36,11 +26,4 @@ fi
 
 "$JAVA_HOME"/bin/java \
     -Dlogging.config="${POLICY_HOME}/etc/logback.xml" \
-    -Dcom.sun.management.jmxremote.rmi.port=9090 \
-    -Dcom.sun.management.jmxremote=true \
-    -Dcom.sun.management.jmxremote.port=9090 \
-    -Dcom.sun.management.jmxremote.ssl=false \
-    -Dcom.sun.management.jmxremote.authenticate=false \
-    -Dcom.sun.management.jmxremote.local.only=false \
-    -jar /app/app.jar \
-    --spring.config.location="${CONFIG_FILE}"
+    -jar /app/app.jar
