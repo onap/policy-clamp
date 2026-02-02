e0c28ae81d8dda9dc7d186b6a6e4d85fc2166f9b
Yali <ngbeyali@yahoo.fr>
2026-02-02 20:28:00 +0100

    Yali5

--- Essai divers/onap-acm-5g-slice-qos-participant/src/main/java/org/onap/acm/participant/a1pms/A1PolicyMapper.java
+++ Essai divers/onap-acm-5g-slice-qos-participant/src/main/java/org/onap/acm/participant/a1pms/A1PolicyMapper.java
@@ -1,3 +1,22 @@
-public class A1PolicyMapper {
-    
+public final class A1PolicyMapper {
+
+    private A1PolicyMapper() {}
+
+    public static A1Policy fromAutomationComposition(
+            ElementContext context) {
+
+        Map<String, Object> props = context.getProperties();
+
+        return A1Policy.builder()
+            .policyId(context.getElementId())
+            .snssai((String) props.get("snssai"))
+            .qos(
+                new QoSProfile(
+                    (Integer) props.get("maxBitrate"),
+                    (Integer) props.get("latency"),
+                    (Integer) props.get("priority")
+                )
+            )
+            .build();
+    }
 }
