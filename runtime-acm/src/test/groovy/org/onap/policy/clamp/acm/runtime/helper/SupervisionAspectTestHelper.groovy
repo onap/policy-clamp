a8ec96483929713fcc453a9614f111d77713667f
KrupaNagabhushan <krupa.nagabhushan@est.tech>
2026-05-20 11:28:23 +0100

    Migrate SupervisionAspectTest into Groovy BDD style
    Issue-ID: POLICY-5578
    Change-Id: I58c1283617d3b653698657d5911d0ed674d0aa21
    Signed-off-by: KrupaNagabhushan <krupa.nagabhushan@est.tech>

---
+++ runtime-acm/src/test/groovy/org/onap/policy/clamp/acm/runtime/helper/SupervisionAspectTestHelper.groovy
@@ -0,0 +1,36 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2026 OpenInfra Foundation Europe. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.policy.clamp.acm.runtime.helper
+
+import org.onap.policy.clamp.acm.runtime.supervision.SupervisionAspect
+import org.onap.policy.clamp.acm.runtime.supervision.SupervisionParticipantScanner
+import org.onap.policy.clamp.acm.runtime.supervision.SupervisionScanner
+
+class SupervisionAspectTestHelper {
+
+    static createAspect(SupervisionScanner scanner,
+                        SupervisionParticipantScanner participantScanner) {
+        return new SupervisionAspect(scanner, participantScanner)
+    }
+
+    static void waitForExecution(int millis = 500) {
+        Thread.sleep(millis)
+    }
+}
