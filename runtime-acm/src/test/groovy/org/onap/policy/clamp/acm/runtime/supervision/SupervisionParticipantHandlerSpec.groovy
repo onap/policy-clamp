73c20a8237109dec85c69af0ea4ca733e4776741
KrupaNagabhushan <krupa.nagabhushan@est.tech>
2026-05-20 12:15:40 +0100

    Migrate SupervisionParticipantHandlerTest into Groovy BDD style
    Issue-ID: POLICY-5578
    Change-Id: Ie9f9437b6ce51befbc8bb6f14e323e81abe6dce4
    Signed-off-by: KrupaNagabhushan <krupa.nagabhushan@est.tech>

---
+++ runtime-acm/src/test/groovy/org/onap/policy/clamp/acm/runtime/supervision/SupervisionParticipantHandlerSpec.groovy
@@ -0,0 +1,328 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2026 OpenInfra Foundation Europe. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.policy.clamp.acm.runtime.supervision
+
+import static org.onap.policy.clamp.acm.runtime.helper.SupervisionParticipantHandlerTestHelper.buildAcDef
+import static org.onap.policy.clamp.acm.runtime.helper.SupervisionParticipantHandlerTestHelper.buildAcDefWithElement
+import static org.onap.policy.clamp.acm.runtime.helper.SupervisionParticipantHandlerTestHelper.buildAcDefWithServiceTemplate
+import static org.onap.policy.clamp.acm.runtime.helper.SupervisionParticipantHandlerTestHelper.buildDeployedAc
+import static org.onap.policy.clamp.acm.runtime.helper.SupervisionParticipantHandlerTestHelper.buildMigratingAc
+import static org.onap.policy.clamp.acm.runtime.helper.SupervisionParticipantHandlerTestHelper.buildRollback
+import static org.onap.policy.clamp.acm.runtime.helper.SupervisionParticipantHandlerTestHelper.createDeregisterMessage
+import static org.onap.policy.clamp.acm.runtime.helper.SupervisionParticipantHandlerTestHelper.createHandler
+import static org.onap.policy.clamp.acm.runtime.helper.SupervisionParticipantHandlerTestHelper.createRegisterMessage
+import static org.onap.policy.clamp.acm.runtime.helper.SupervisionParticipantHandlerTestHelper.createReqSyncMessage
+import static org.onap.policy.clamp.acm.runtime.helper.SupervisionParticipantHandlerTestHelper.createStatusMessage
+import static org.onap.policy.clamp.acm.runtime.helper.SupervisionParticipantHandlerTestHelper.createStatusMessageWithParticipantDef
+
+import org.onap.policy.clamp.acm.runtime.main.utils.EncryptionUtils
+import org.onap.policy.clamp.acm.runtime.supervision.comm.ParticipantDeregisterAckPublisher
+import org.onap.policy.clamp.acm.runtime.supervision.comm.ParticipantRegisterAckPublisher
+import org.onap.policy.clamp.acm.runtime.supervision.comm.ParticipantSyncPublisher
+import org.onap.policy.clamp.acm.runtime.util.CommonTestData
+import org.onap.policy.clamp.models.acm.concepts.AutomationCompositionInfo
+import org.onap.policy.clamp.models.acm.concepts.ParticipantReplica
+import org.onap.policy.clamp.models.acm.concepts.ParticipantState
+import org.onap.policy.clamp.models.acm.persistence.provider.AcDefinitionProvider
+import org.onap.policy.clamp.models.acm.persistence.provider.AutomationCompositionProvider
+import org.onap.policy.clamp.models.acm.persistence.provider.MessageProvider
+import org.onap.policy.clamp.models.acm.persistence.provider.ParticipantProvider
+import spock.lang.Specification
+
+class SupervisionParticipantHandlerSpec extends Specification {
+
+    static final PARTICIPANT_ID = CommonTestData.getParticipantId()
+    static final REPLICA_ID = CommonTestData.getReplicaId()
+
+    // ---- Deregister ----
+
+    def "deregister '#desc' should send ack"() {
+        given:
+        def deregisterAck = Mock(ParticipantDeregisterAckPublisher)
+        def participantProvider = Mock(ParticipantProvider)
+        def handler = buildHandler(
+                participantProvider: participantProvider,
+                deregisterAckPublisher: deregisterAck)
+        def msg = createDeregisterMessage(msgParams)
+
+        when:
+        handler.handleParticipantMessage(msg)
+
+        then:
+        1 * participantProvider.findParticipantReplica(lookupId) >>
+                replicaOpt
+        deleteCount * participantProvider
+                .deleteParticipantReplica(lookupId)
+        1 * deregisterAck.send(msg.messageId)
+
+        where:
+        desc             | msgParams
+        "no replicaId"   | [:]
+        "with replicaId" | [replicaId: REPLICA_ID]
+
+        lookupId = msgParams.replicaId ?: PARTICIPANT_ID
+        replicaOpt = msgParams.replicaId
+                ? Optional.of(CommonTestData
+                        .createParticipantReplica(REPLICA_ID))
+                : Optional.empty()
+        deleteCount = msgParams.replicaId ? 1 : 0
+    }
+
+    // ---- Register ----
+
+    def "register new participant should save and send ack"() {
+        given:
+        def participantProvider = Mock(ParticipantProvider)
+        def registerAck = Mock(ParticipantRegisterAckPublisher)
+        def handler = buildHandler(
+                participantProvider: participantProvider,
+                registerAckPublisher: registerAck)
+        def msg = createRegisterMessage()
+
+        when:
+        handler.handleParticipantMessage(msg)
+
+        then:
+        1 * participantProvider.findParticipantReplica(PARTICIPANT_ID) >>
+                Optional.empty()
+        1 * participantProvider.findParticipant(PARTICIPANT_ID) >>
+                Optional.empty()
+        1 * participantProvider.saveParticipant(_)
+        1 * participantProvider.getCompositionIds(PARTICIPANT_ID) >>
+                Collections.emptySet()
+        1 * registerAck.send(msg.messageId, PARTICIPANT_ID, null)
+    }
+
+    def "register existing replica should ack without restart"() {
+        given:
+        def participantProvider = Mock(ParticipantProvider)
+        def registerAck = Mock(ParticipantRegisterAckPublisher)
+        def syncPublisher = Mock(ParticipantSyncPublisher)
+        def acDefinitionProvider = Mock(AcDefinitionProvider)
+        def replica = new ParticipantReplica(
+                replicaId: REPLICA_ID,
+                participantState: ParticipantState.OFF_LINE)
+        def handler = buildHandler(
+                participantProvider: participantProvider,
+                registerAckPublisher: registerAck,
+                acDefinitionProvider: acDefinitionProvider,
+                syncPublisher: syncPublisher)
+        def msg = createRegisterMessage(replicaId: REPLICA_ID)
+
+        when:
+        handler.handleParticipantMessage(msg)
+
+        then:
+        1 * participantProvider.findParticipantReplica(REPLICA_ID) >>
+                Optional.of(replica)
+        1 * participantProvider.saveParticipantReplica(replica)
+        1 * registerAck.send(msg.messageId, PARTICIPANT_ID, REPLICA_ID)
+        0 * acDefinitionProvider.updateAcDefinition(_, _)
+        0 * syncPublisher.sendRestartMsg(_, _, _, _)
+    }
+
+    // ---- Status ----
+
+    def "status with instance out properties should save them"() {
+        given:
+        def participantProvider = Mock(ParticipantProvider)
+        def messageProvider = Mock(MessageProvider)
+        def replica = CommonTestData.createParticipantReplica(REPLICA_ID)
+        def handler = buildHandler(
+                participantProvider: participantProvider,
+                messageProvider: messageProvider)
+        def msg = createStatusMessage(
+                compositionId: UUID.randomUUID(),
+                replicaId: REPLICA_ID)
+        msg.automationCompositionInfoList = [
+                new AutomationCompositionInfo()]
+
+        when:
+        handler.handleParticipantMessage(msg)
+
+        then:
+        1 * participantProvider.findParticipantReplica(REPLICA_ID) >>
+                Optional.of(replica)
+        1 * participantProvider.saveParticipantReplica(replica)
+        1 * messageProvider.saveInstanceOutProperties(_)
+    }
+
+    def "status composition out '#desc'"() {
+        given:
+        def participantProvider = Mock(ParticipantProvider)
+        def messageProvider = Mock(MessageProvider)
+        def acDefinitionProvider = Mock(AcDefinitionProvider)
+        def compositionId = UUID.randomUUID()
+        def handler = buildHandler(
+                participantProvider: participantProvider,
+                acDefinitionProvider: acDefinitionProvider,
+                messageProvider: messageProvider)
+        def msg = createStatusMessageWithParticipantDef(
+                compositionId, PARTICIPANT_ID)
+
+        when:
+        handler.handleParticipantMessage(msg)
+
+        then:
+        1 * participantProvider.findParticipantReplica(PARTICIPANT_ID) >>
+                Optional.empty()
+        1 * participantProvider.findParticipant(PARTICIPANT_ID) >>
+                Optional.empty()
+        1 * participantProvider.saveParticipant(_)
+        1 * acDefinitionProvider.findAcDefinition(compositionId) >>
+                definitionOpt
+        saveCount * messageProvider.saveCompositionOutProperties(_, _)
+
+        where:
+        desc        | definitionOpt                             | saveCount
+        "found"     | Optional.of(buildAcDefWithElement("code"))| 1
+        "not found" | Optional.empty()                          | 0
+    }
+
+    def "status '#desc' should handle participant"() {
+        given:
+        def participantProvider = Mock(ParticipantProvider)
+        def handler = buildHandler(
+                participantProvider: participantProvider)
+        def msg = createStatusMessage(compositionId: UUID.randomUUID())
+        msg.automationCompositionInfoList = []
+
+        when:
+        handler.handleParticipantMessage(msg)
+
+        then:
+        1 * participantProvider.findParticipantReplica(PARTICIPANT_ID) >>
+                replicaOpt
+        saveReplicaCount * participantProvider.saveParticipantReplica(_)
+        savePartCount * participantProvider.saveParticipant(_)
+        findPartCount * participantProvider
+                .findParticipant(PARTICIPANT_ID) >> Optional.empty()
+
+        where:
+        desc             | registered | saveReplicaCount | savePartCount | findPartCount
+        "not registered" | false      | 0                | 1             | 1
+        "check online"   | true       | 1                | 0             | 0
+
+        replicaOpt = registered
+                ? Optional.of(CommonTestData
+                        .createParticipantReplica(PARTICIPANT_ID))
+                : Optional.empty()
+    }
+
+    // ---- ReqSync ----
+
+    def "reqSync '#desc' should send restart with empty AC list"() {
+        given:
+        def acDefinitionProvider = Mock(AcDefinitionProvider)
+        def syncPublisher = Mock(ParticipantSyncPublisher)
+        def compositionId = UUID.randomUUID()
+        def acDef = buildAcDef(compositionId)
+        def handler = buildHandler(
+                acDefinitionProvider: acDefinitionProvider,
+                syncPublisher: syncPublisher)
+        def msg = createReqSyncMessage(msgParams(compositionId))
+
+        when:
+        handler.handleParticipantReqSync(msg)
+
+        then:
+        1 * acDefinitionProvider.getAcDefinition(compositionId) >> acDef
+        1 * syncPublisher.sendRestartMsg(
+                PARTICIPANT_ID, null, acDef, [])
+
+        where:
+        desc                  | msgParams
+        "compositionId"       | { id -> [compositionId: id] }
+        "compositionTargetId" | { id -> [compositionTargetId: id] }
+    }
+
+    def "reqSync with compositionId and instanceId should include AC"() {
+        given:
+        def acDefinitionProvider = Mock(AcDefinitionProvider)
+        def syncPublisher = Mock(ParticipantSyncPublisher)
+        def acProvider = Mock(AutomationCompositionProvider)
+        def compositionId = UUID.randomUUID()
+        def acDef = buildAcDef(compositionId)
+        def ac = buildDeployedAc(compositionId)
+        def handler = buildHandler(
+                acProvider: acProvider,
+                acDefinitionProvider: acDefinitionProvider,
+                syncPublisher: syncPublisher)
+        def msg = createReqSyncMessage(
+                compositionId: compositionId,
+                automationCompositionId: ac.instanceId)
+
+        when:
+        handler.handleParticipantReqSync(msg)
+
+        then:
+        1 * acDefinitionProvider.getAcDefinition(compositionId) >> acDef
+        1 * acProvider.getAutomationComposition(ac.instanceId) >> ac
+        1 * syncPublisher.sendRestartMsg(
+                PARTICIPANT_ID, null, acDef, [ac])
+    }
+
+    def "reqSync MIGRATING '#desc' should send sync"() {
+        given:
+        def acDefinitionProvider = Mock(AcDefinitionProvider)
+        def syncPublisher = Mock(ParticipantSyncPublisher)
+        def acProvider = Mock(AutomationCompositionProvider)
+        def compositionId = UUID.randomUUID()
+        def acDef = buildAcDefWithServiceTemplate(compositionId)
+        def ac = buildMigratingAc(compositionId, phase)
+        def handler = buildHandler(
+                acProvider: acProvider,
+                acDefinitionProvider: acDefinitionProvider,
+                syncPublisher: syncPublisher)
+        def msg = createReqSyncMessage(
+                automationCompositionId: ac.instanceId)
+
+        when:
+        handler.handleParticipantReqSync(msg)
+
+        then:
+        1 * acProvider.getAutomationComposition(ac.instanceId) >> ac
+        1 * acDefinitionProvider.getAcDefinition(compositionId) >> acDef
+        rollbackCount * acProvider.getAutomationCompositionRollback(
+                ac.instanceId) >> buildRollback()
+        1 * syncPublisher.sendSync(ac)
+
+        where:
+        desc                  | phase | rollbackCount
+        "not at first stage"  | 1     | 0
+        "at first stage"      | 0     | 1
+    }
+
+    // ---- Helpers ----
+
+    def buildHandler(Map overrides) {
+        def defaults = [
+                participantProvider: Mock(ParticipantProvider),
+                registerAckPublisher: Mock(ParticipantRegisterAckPublisher),
+                deregisterAckPublisher: Mock(ParticipantDeregisterAckPublisher),
+                acProvider: Mock(AutomationCompositionProvider),
+                acDefinitionProvider: Mock(AcDefinitionProvider),
+                syncPublisher: Mock(ParticipantSyncPublisher),
+                messageProvider: Mock(MessageProvider),
+                encryptionUtils: Mock(EncryptionUtils)
+        ]
+        createHandler(defaults + overrides)
+    }
+}
