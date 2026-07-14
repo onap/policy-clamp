cda6646407f397499b726dba6816caa6416888ba
FrancescoFioraEst <francesco.fiora@est.tech>
2026-07-14 10:09:29 +0100

    Add CompositionDto endpoint in ACM-r
    Issue-ID: POLICY-5629
    Change-Id: I1edf87931de2f29054898e19cfe1750e5fb632bf
    Signed-off-by: FrancescoFioraEst <francesco.fiora@est.tech>

---
+++ runtime-acm/src/main/java/org/onap/policy/clamp/acm/runtime/commissioning/CompositionDtoService.java
@@ -0,0 +1,45 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2026 OpenInfra Foundation Europe. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.clamp.acm.runtime.commissioning;
+
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.onap.policy.clamp.models.acm.dto.CompositionDto;
+import org.onap.policy.clamp.models.acm.dto.CompositionDtos;
+import org.onap.policy.clamp.models.acm.persistence.provider.CompositionDtoProvider;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CompositionDtoService {
+
+    private final CompositionDtoProvider compositionDtoProvider;
+
+    public CompositionDtos getCompositionDtos(UUID participantId, Pageable pageable) {
+        var result = compositionDtoProvider.getCompositionDtoList(participantId, pageable);
+        return new CompositionDtos(result);
+    }
+
+    public CompositionDto getCompositionDto(UUID participantId, UUID compositionId) {
+        return compositionDtoProvider.getCompositionDto(participantId, compositionId);
+    }
+}
