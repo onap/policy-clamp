c9013759764d45b10ca82efd33c0479a3ca36ec4
rameshiyer27 <ramesh.murugan.iyer@est.tech>
2026-07-29 14:23:35 +0100

    Remove sensitive data loggers from Clamp
    Issue-ID: POLICY-5650
    Signed-off-by: rameshiyer27 <ramesh.murugan.iyer@est.tech>
    Change-Id: I3674234826d63be7bdcf7d22f0e26eece2c3cfcb

--- policy-common/src/main/java/org/onap/policy/common/endpoints/listeners/ScoListener.java
+++ policy-common/src/main/java/org/onap/policy/common/endpoints/listeners/ScoListener.java
@@ -4,7 +4,7 @@
  * ================================================================================
  * Copyright (C) 2019, 2021 AT&T Intellectual Property. All rights reserved.
  * Modifications Copyright (C) 2020 Bell Canada. All rights reserved.
- * Modifications Copyright (C) 2024 Nordix Foundation.
+ * Modifications Copyright (C) 2024,2026 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,7 +68,7 @@ public abstract class ScoListener<T> {
             msg = coder.fromStandard(sco, clazz);
 
         } catch (CoderException e) {
-            logger.warn("unable to decode {}: {}", clazz.getName(), sco, e);
+            logger.warn("unable to decode {} on topic [{}]", clazz.getName(), topic, e);
             return;
         }
 
